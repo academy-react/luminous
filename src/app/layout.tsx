37ec992552d85885cb258657627375c724b946e9
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-09-30 00:43:51 +0330

    chore: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "@/styles/globals.css";
+
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Luminous",
+  description: "The Luminous Team React Project",
+  icons: {
+    icon: "/favicon/favicon.ico",
+  },
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className="min-h-screen h-screen">{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <main className="flex h-full items-center justify-center">
+      <h1 className="font-mono">The Luminous Team React Project</h1>
+    </main>
+  );
+};
+
+export default HomePage;
