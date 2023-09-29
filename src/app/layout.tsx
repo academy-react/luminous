786b5296eccf672c7fae82ae8a2ceee43fbdd89b
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-09-30 01:21:21 +0330

    chore: tailwind indicator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 
 import { type Metadata } from "next";
 
+import TailwindIndicator from "@/components/utils/tailwind-indicator";
+
 export const metadata: Metadata = {
   title: "Luminous",
   description: "The Luminous Team React Project",
@@ -13,7 +15,10 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body className="h-screen min-h-screen">{children}</body>
+      <body className="h-screen min-h-screen">
+        {children}
+        <TailwindIndicator />
+      </body>
     </html>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,15 @@
-import type { Config } from "tailwindcss";
+import { type Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
+  theme: {
+    extend: {
+      screens: {
+        xs: "480px",
+        xxs: "350px",
+      },
+    },
+  },
   plugins: [],
 };
 
