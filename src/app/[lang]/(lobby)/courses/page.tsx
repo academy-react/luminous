dee0b8ce1060db0adfdeba7416cd3d40f89c9368
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-10-10 15:09:26 +0330

    fix: removed unused components

--- src/app/[lang]/(lobby)/courses/page.tsx
+++ src/app/[lang]/(lobby)/courses/page.tsx
@@ -1,12 +1,9 @@
-import CourseCard from "@/components/layout/courses/course-card";
-
 import { type Locale } from "#/i18n.config";
 
 const CoursesPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
   return (
     <main className="flex h-full items-center justify-center">
       <h1 className="text-xl">صفحه‌ی کورس‌ها</h1>
-      <CourseCard />
     </main>
   );
 };
