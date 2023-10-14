a916056e4fb777ed7e597c44df35ae50c080f6ab
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-14 16:44:51 -0700

    feat:add service-section component to page

--- src/app/[lang]/(lobby)/page.tsx
+++ src/app/[lang]/(lobby)/page.tsx
@@ -1,4 +1,7 @@
+import { Home } from "lucide-react";
+
 import HeroSection from "@/components/lobby/hero/hero-section";
+import ServiceSection from "@/components/lobby/services/service-section";
 import ThemeSwitcher from "@/components/utils/theme-switcher";
 
 import { type Locale } from "#/i18n.config";
@@ -9,6 +12,7 @@ const HomePage = ({ params: { lang } }: { params: { lang: Locale } }) => {
       <div className="h-fit w-full">
         <HeroSection lang={lang} />
       </div>
+      <ServiceSection lang={lang} />
       <ThemeSwitcher lang={lang} />
     </main>
   );
