7fbd4f3141a8710f1a9666e1d787e748bbdb8c56
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-15 09:44:19 -0700

    refactor:add courseSection to landing for test

--- src/app/[lang]/(lobby)/page.tsx
+++ src/app/[lang]/(lobby)/page.tsx
@@ -1,3 +1,4 @@
+import CourseSection from "@/components/lobby/courses/course-section";
 import HeroSection from "@/components/lobby/hero/hero-section";
 import ThemeSwitcher from "@/components/utils/theme-switcher";
 
@@ -9,6 +10,14 @@ const HomePage = ({ params: { lang } }: { params: { lang: Locale } }) => {
       <div className="h-fit w-full">
         <HeroSection lang={lang} />
       </div>
+      {/* courseSection */}
+      <div className="flex  w-full border border-black px-20">
+        <div className="w-[70%]  border border-black">
+          <CourseSection />
+        </div>
+        <div className="w-[30%]  border border-black">2</div>
+      </div>
+
       <ThemeSwitcher lang={lang} />
     </main>
   );
