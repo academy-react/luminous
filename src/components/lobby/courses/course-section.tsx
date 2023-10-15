1346baa4d773cc5de2fbf2c8b72f1f3bd8510582
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-15 09:43:19 -0700

    feat:create courseSection component

---
+++ src/components/lobby/courses/course-section.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const CourseSection = () => {
+  return (
+    <></>
+
+    //   <div className="h-[350px]   w-[470px] rounded-xl border border-purple-primary shadow-lg">
+    //     <Link href="#">
+    //       <Image
+    //         src="/images/node jd.svg"
+    //         alt=""
+    //         width={450}
+    //         height={200}
+    //         className="mx-auto pt-2"
+    //       />
+    //     </Link>
+    //     <div className="relative mx-auto flex w-11/12 flex-col ">
+    //       <div className=" flex justify-between">
+    //         <Link href="#">
+    //           <h2>نود جی اس:دوره کامل</h2>
+    //         </Link>
+    //         <div>آرمان ساکت</div>
+    //       </div>
+    //       <p className="line-clamp-2">
+    //         در کادر زیر هر متنی را که دوست دارید تایپ کنید تا ما آن را برایتان
+    //         نگه داریم و همیشه در دسترس شما قرار دهیم؛ ا استفاده کنید
+    //       </p>
+    //       {/* <Button
+    //       href={"#"}
+    //       style={"absolute left-5 border border-purple-primary"}
+    //       title={"جزییات دوره"}
+    //     /> */}
+    //       <Link
+    //         href="#"
+    //         className="absolute left-5 border border-purple-primary"
+    //       >
+    //         جزییات دوره
+    //       </Link>
+    //     </div>
+    //   </div>
+  );
+};
+export default CourseSection;
