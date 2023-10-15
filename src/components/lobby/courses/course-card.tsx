703fb0dec559508325889c9a66363f3c6a68e2f7
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-15 09:44:45 -0700

    feat:create courseCard

---
+++ src/components/lobby/courses/course-card.tsx
@@ -0,0 +1,17 @@
+const CourseCard = () => {
+  return (
+    <ul className="flex h-full w-full flex-col border border-red-400">
+      <li className="">1</li>
+      <ul className="mx-auto flex h-20 w-full items-center justify-between border border-blue-400">
+        <li>1</li>
+        <li>2</li>
+      </ul>
+      <ul className="mx-auto flex h-20 w-full items-center justify-between border border-blue-400">
+        <li>1</li>
+        <li>2</li>
+      </ul>
+    </ul>
+  );
+};
+
+export default CourseCard;
