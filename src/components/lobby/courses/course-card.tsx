57cdf51d30339212904cdd51e5a30ed72c8e5f78
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-15 15:16:49 -0700

    refactor:modify course-card

--- src/components/lobby/courses/course-card.tsx
+++ src/components/lobby/courses/course-card.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 
 const CourseCard = () => {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center gap-4 border border-red-400 py-3">
+    <div className="flex h-full w-full flex-col  gap-4  py-3">
       <Link href="#" className="relative aspect-[2/1] w-full ">
         <Image
           src="/images/node jd.svg"
@@ -14,16 +14,16 @@ const CourseCard = () => {
           className=" object-contain "
         />
       </Link>
-      <div className=" flex w-full items-center justify-between border border-blue-400 px-5">
+      <div className=" flex w-full items-center justify-between px-5">
         <h2 className="text-2xl font-bold">نود جی اس:دوره کامل</h2>
         <h4 className="text-lg text-[#707070]">آرمین ساکت</h4>
       </div>
-      <div className=" flex w-full  items-center justify-between border border-blue-400 px-5">
+      <div className=" flex w-full  items-end justify-between px-5">
         <p className="w-[70%] text-base text-[#707070]">
           متون کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذش سوالات پیوسته
           اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
         </p>
-        <Button className="rounded-xl border-2 border-purple-primary p-1 text-purple-primary ">
+        <Button className=" rounded-xl border-2 border-purple-primary p-1 text-purple-primary ">
           <Link href="#" className="">
             مشاهده بیشتر
           </Link>
