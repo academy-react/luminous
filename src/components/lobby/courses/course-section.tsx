1ffb34322192b5a4dbfca73ad06cd74e7293d540
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-15 15:17:16 -0700

    refactor:modify course-section

--- src/components/lobby/courses/course-section.tsx
+++ src/components/lobby/courses/course-section.tsx
@@ -1,44 +1,22 @@
-import Image from "next/image";
-import Link from "next/link";
+import CourseCard from "./course-card";
+import SideCard from "./course-side-card";
 
 const CourseSection = () => {
   return (
-    <></>
-
-    //   <div className="h-[350px]   w-[470px] rounded-xl border border-purple-primary shadow-lg">
-    //     <Link href="#">
-    //       <Image
-    //         src="/images/node jd.svg"
-    //         alt=""
-    //         width={450}
-    //         height={200}
-    //         className="mx-auto pt-2"
-    //       />
-    //     </Link>
-    //     <div className="relative mx-auto flex w-11/12 flex-col ">
-    //       <div className=" flex justify-between">
-    //         <Link href="#">
-    //           <h2>نود جی اس:دوره کامل</h2>
-    //         </Link>
-    //         <div>آرمان ساکت</div>
-    //       </div>
-    //       <p className="line-clamp-2">
-    //         در کادر زیر هر متنی را که دوست دارید تایپ کنید تا ما آن را برایتان
-    //         نگه داریم و همیشه در دسترس شما قرار دهیم؛ ا استفاده کنید
-    //       </p>
-    //       {/* <Button
-    //       href={"#"}
-    //       style={"absolute left-5 border border-purple-primary"}
-    //       title={"جزییات دوره"}
-    //     /> */}
-    //       <Link
-    //         href="#"
-    //         className="absolute left-5 border border-purple-primary"
-    //       >
-    //         جزییات دوره
-    //       </Link>
-    //     </div>
-    //   </div>
+    <>
+      <div className="flex  w-full gap-8 px-28">
+        <div className="w-[70%]  rounded-xl shadow-xl">
+          <CourseCard />
+        </div>
+        <div className="w-[30%]  rounded-xl shadow-xl">
+          <div className="flex h-full w-full flex-col gap-3 p-3">
+            <SideCard />
+            <SideCard />
+            <SideCard />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 export default CourseSection;
