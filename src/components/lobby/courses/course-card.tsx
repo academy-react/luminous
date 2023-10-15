c7e2e92125501dcf834593d1097e13a91f2a5b3d
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-15 14:09:43 -0700

    refactor:courseCard

--- src/components/lobby/courses/course-card.tsx
+++ src/components/lobby/courses/course-card.tsx
@@ -1,16 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 const CourseCard = () => {
   return (
-    <ul className="flex h-full w-full flex-col border border-red-400">
-      <li className="">1</li>
-      <ul className="mx-auto flex h-20 w-full items-center justify-between border border-blue-400">
-        <li>1</li>
-        <li>2</li>
-      </ul>
-      <ul className="mx-auto flex h-20 w-full items-center justify-between border border-blue-400">
-        <li>1</li>
-        <li>2</li>
-      </ul>
-    </ul>
+    <div className="flex h-full w-full flex-col items-center justify-center gap-4 border border-red-400 py-3">
+      <Link href="#" className="relative aspect-[2/1] w-full ">
+        <Image
+          src="/images/node jd.svg"
+          alt=""
+          fill
+          className=" object-contain "
+        />
+      </Link>
+      <div className=" flex w-full items-center justify-between border border-blue-400 px-5">
+        <h2 className="text-2xl font-bold">نود جی اس:دوره کامل</h2>
+        <h4 className="text-lg text-[#707070]">آرمین ساکت</h4>
+      </div>
+      <div className=" flex w-full  items-center justify-between border border-blue-400 px-5">
+        <p className="w-[70%] text-base text-[#707070]">
+          متون کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذش سوالات پیوسته
+          اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+        </p>
+        <Button className="rounded-xl border-2 border-purple-primary p-1 text-purple-primary ">
+          <Link href="#" className="">
+            مشاهده بیشتر
+          </Link>
+        </Button>
+      </div>
+    </div>
   );
 };
 
