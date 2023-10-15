b025d514a0d732914382d0652d9d32bd9b784e56
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-14 17:47:59 -0700

    refactor:service-section

--- src/components/lobby/services/service-section.tsx
+++ src/components/lobby/services/service-section.tsx
@@ -1,6 +1,6 @@
 import { services } from "@/dict/lobby/services.dict";
 
-import { Card, CardContent } from "@/components/ui/card";
+import { CardContent } from "@/components/ui/card";
 import TitleBox from "@/components/common/landing-title-box";
 
 import { type Locale } from "#/i18n.config";
@@ -10,8 +10,8 @@ import ServiceCard from "./service-card";
 const ServiceSection = ({ lang }: { lang: Locale }) => {
   return (
     <div>
-      <Card className="flex w-full flex-col  ">
-        <TitleBox />
+      <div className="flex w-full flex-col  ">
+        <TitleBox title="خدمات " desc="تمام آنچه نیاز دارید" className="" />
         <CardContent className="grid w-full grid-cols-2 gap-x-8 gap-y-6 ">
           {services.map((item) => (
             <ServiceCard
@@ -21,7 +21,7 @@ const ServiceSection = ({ lang }: { lang: Locale }) => {
             />
           ))}
         </CardContent>
-      </Card>
+      </div>
     </div>
   );
 };
