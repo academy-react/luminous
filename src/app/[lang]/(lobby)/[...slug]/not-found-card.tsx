ab8bb8ad8398cca978c43ee33275ad58dd2b5e2e
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-16 01:38:38 -0700

    refactor:not-found-card completed

--- src/app/[lang]/(lobby)/[...slug]/not-found-card.tsx
+++ src/app/[lang]/(lobby)/[...slug]/not-found-card.tsx
@@ -1,4 +1,28 @@
-const NotFoundCard = () => {
-  return <></>;
+import Image from "next/image";
+import Link from "next/link";
+
+import { type Locale } from "#/i18n.config";
+
+const NotFoundCard = ({ lang }: { lang: Locale }) => {
+  return (
+    <>
+      <div className="mx-auto  flex w-1/3 flex-col  ">
+        <div className="relative aspect-[2/1] w-full ">
+          <Image
+            alt="404-bg"
+            src="/images/artworks/404.svg"
+            fill
+            className="object-contain"
+          />
+        </div>
+        <Link
+          href={`/${lang}`}
+          className="mx-auto rounded-xl border-2 bg-purple-primary px-4 py-1 text-base text-white"
+        >
+          بازگشت به صفحه اصلی
+        </Link>
+      </div>
+    </>
+  );
 };
 export default NotFoundCard;
