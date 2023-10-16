5ea7d0101b4faf298fa5a31590418ad9ac4be181
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-16 01:39:32 -0700

    refactor:not-found-page modified

--- src/app/[lang]/(lobby)/[...slug]/page.tsx
+++ src/app/[lang]/(lobby)/[...slug]/page.tsx
@@ -1,4 +1,12 @@
-const NotFound = () => {
-  return <></>;
+import {type  Locale } from "#/i18n.config";
+
+import NotFoundCard from "./not-found-card";
+
+const NotFound = ({ lang }: { lang: Locale }) => {
+  return (
+    <>
+      <NotFoundCard lang={lang} />
+    </>
+  );
 };
 export default NotFound;
