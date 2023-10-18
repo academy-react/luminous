9f1e6ed47699e45dd4328addfd79ea215157507a
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-17 22:42:19 -0700

    refactor:testing cards in this page

--- src/app/[lang]/(lobby)/courses/page.tsx
+++ src/app/[lang]/(lobby)/courses/page.tsx
@@ -1,11 +1,10 @@
-import CourseFullCard from "@/components/course-page/course-full-card";
 
 import { type Locale } from "#/i18n.config";
 
 const CoursesPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
   return (
     <main className="flex h-full items-center justify-center px-36">
-      <CourseFullCard />
+  
     </main>
   );
 };
