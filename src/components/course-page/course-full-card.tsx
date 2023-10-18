a9394cebbbf6208b5dc1718413cba7c38658e3ca
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-17 22:41:24 -0700

    refactor:remove borders-course-full-card

--- src/components/course-page/course-full-card.tsx
+++ src/components/course-page/course-full-card.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const CourseFullCard = () => {
   return (
-    <div className="flex h-[200px] w-full  gap-4 rounded-xl p-4 shadow-lg">
-      <Link href="#" className="relative aspect-square basis-1/3 ">
+    <div className="flex w-full  gap-4 rounded-xl p-4 shadow-lg">
+      <Link href="#" className="relative aspect-[2/1] basis-1/3 ">
         <Image
           src="/images/node jd.svg"
           alt=""
@@ -12,29 +12,31 @@ const CourseFullCard = () => {
           className="rounded-xl object-cover"
         />
       </Link>
-      <div className="h-full basis-2/3 border border-black ">
-        <div className="flex justify-between border border-black ">
-          <div className="flex flex-col gap-2 border border-black">
+      <div className=" basis-2/3">
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-2">
             <h2 className="text-2xl font-bold">نود جی اس:دوره کامل</h2>
             <p className="w-[70%] text-base text-[#707070]">
               متون کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذش سوالات
               پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
             </p>
           </div>
-          <ul className="flex flex-col rounded-xl border border-black ">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
+          <ul className="flex flex-col  rounded-xl px-2 shadow-lg">
+            <li className="">1</li>
+            <li className=" ">20</li>
+            <li className="">30</li>
           </ul>
         </div>
-        <div className="flex items-end  justify-between  border border-black  pt-10">
+        <div className="flex items-end  justify-between">
           <ul className="flex gap-4">
             <li>مدرس:استاد بحرالعلوم</li>
             <li>مدت زمان دوره:80ساعت</li>
             <li>*****</li>
           </ul>
 
-          <span>8500000تومان</span>
+          <span className="rounded-xl border-2 border-[#58AD57]  px-2  text-[#58AD57] ">
+            8500000تومان
+          </span>
         </div>
       </div>
     </div>
