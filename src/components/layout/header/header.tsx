382d9bad2d908101071c21677fa1986a42b699b3
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-10-19 15:28:11 +0330

    refactor: header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,69 +12,92 @@ import { headerMenuOptions } from "@/dict/lobby/header.dict";
 import type { Locale } from "#/i18n.config";
 
 const Header = ({ lang }: { lang: Locale }) => {
+  const [backgroundColor, setBackgroundColor] = useState("bg-transparent");
+
+  const handleScroll = () => {
+    const scrollPosition = window.pageYOffset;
+
+    if (scrollPosition > 0) {
+      setBackgroundColor("bg-white dark:bg-black shadow");
+    } else {
+      setBackgroundColor("bg-transparent");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
-      <nav className="fixed z-50 mx-auto flex h-16 w-full items-center justify-between px-4 py-3 sm:px-5 lg:h-20 lg:px-10 lg:py-5">
-        {/* Nav Bar */}
-        <div className="flex h-full items-center gap-4">
-          <Link href={`/${lang}`} className="relative aspect-square h-full">
-            <Image
-              src="/images/logos/logo.svg"
-              fill
-              alt="website-logo"
-              className="object-cover"
-            />
-          </Link>
-          <div className="hidden w-full md:block md:w-auto">
-            <ul className="flex gap-6">
-              {headerMenuOptions.map((item) => (
-                <li key={item.id}>
-                  <Link
-                    href={`/${lang}/${item.href}`}
-                    className={cn(
-                      "font-semibold text-purple-text decoration-1 underline-offset-[12px] hover:underline",
-                      lang === "en"
-                        ? "text-base capitalize lg:text-lg"
-                        : "text-lg lg:text-xl"
-                    )}
-                  >
-                    {item.title[lang]}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
+    <header
+      className={cn(
+        "fixed z-50 mx-auto flex h-16 w-full items-center justify-between px-4 py-3 transition-all duration-300 sm:px-5 lg:h-20 lg:px-10 lg:py-5",
+        backgroundColor
+      )}
+    >
+      {/* Routes Nav Bar */}
+      <nav className="flex h-full items-center gap-4">
+        <Link href={`/${lang}`} className="relative aspect-square h-full">
+          <Image
+            src="/images/logos/logo.svg"
+            fill
+            alt="website-logo"
+            className="object-cover"
+          />
+        </Link>
+        <div className="hidden w-full md:block md:w-auto">
+          <ul className="flex gap-6">
+            {headerMenuOptions.map((item) => (
+              <li key={item.id}>
+                <Link
+                  href={`/${lang}/${item.href}`}
+                  className={cn(
+                    "font-semibold text-purple-text decoration-1 underline-offset-[12px] hover:underline",
+                    lang === "en"
+                      ? "text-base capitalize lg:text-lg"
+                      : "text-lg lg:text-xl"
+                  )}
+                >
+                  {item.title[lang]}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
-        {/* Auth Nav Bar */}
-        <div className="flex h-full">
-          {/* Mobile Menu */}
-          <button className="h-full rounded-lg text-gray-300 transition-all duration-500 hover:text-black md:hidden">
-            <Menu />
-          </button>
-          {/* Desktop Menu */}
-          <div className="hidden items-center gap-2 md:flex">
-            <div className="flex aspect-square h-full cursor-pointer items-center justify-center rounded-xl bg-purple-btn text-purple-text transition hover:bg-white">
-              <Search />
-            </div>
-            <div
-              className={cn(
-                "flex h-full gap-2",
-                lang === "en" ? "text-base lg:text-lg" : "text-lg lg:text-xl"
-              )}
+      </nav>
+      {/* Auth Nav Bar */}
+      <nav className="flex h-full">
+        {/* Mobile Menu */}
+        <button className="h-full rounded-lg text-gray-300 transition-all duration-500 hover:text-black md:hidden">
+          <Menu />
+        </button>
+        {/* Desktop Menu */}
+        <div className="hidden items-center gap-2 md:flex">
+          <div className="flex aspect-square h-full cursor-pointer items-center justify-center rounded-xl bg-purple-btn text-purple-text transition hover:bg-white">
+            <Search />
+          </div>
+          <div
+            className={cn(
+              "flex h-full gap-2",
+              lang === "en" ? "text-base lg:text-lg" : "text-lg lg:text-xl"
+            )}
+          >
+            <Link
+              href={`/${lang}/sign-up`}
+              className="flex h-full items-center justify-center rounded-xl bg-purple-btn px-7 text-center text-purple-text transition hover:bg-white"
             >
-              <Link
-                href={`/${lang}/sign-up`}
-                className="flex h-full items-center justify-center rounded-xl bg-purple-btn px-7 text-center text-purple-text transition hover:bg-white"
-              >
-                {{ fa: "ثبت‌نام", en: "Sign Up" }[lang]}
-              </Link>
-              <Link
-                href={`/${lang}/sign-in`}
-                className="flex h-full items-center justify-center rounded-xl bg-purple-btn px-7 text-center text-purple-text transition hover:bg-white"
-              >
-                {{ fa: "ورود", en: "Sign In" }[lang]}
-              </Link>
-            </div>
+              {{ fa: "ثبت‌نام", en: "Sign Up" }[lang]}
+            </Link>
+            <Link
+              href={`/${lang}/sign-in`}
+              className="flex h-full items-center justify-center rounded-xl bg-purple-btn px-7 text-center text-purple-text transition hover:bg-white"
+            >
+              {{ fa: "ورود", en: "Sign In" }[lang]}
+            </Link>
           </div>
         </div>
       </nav>
