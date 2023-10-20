9815d6baa4bdccfa1a0f1e29984e1d8d2b627b2f
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-10-20 17:07:19 +0330

    refactor: service cards

--- src/components/lobby/services/service-card.tsx
+++ src/components/lobby/services/service-card.tsx
@@ -12,9 +12,9 @@ type ServiceCardProps = {
 
 const ServiceCard = ({ Icon, title, bgColor }: ServiceCardProps) => {
   return (
-    <Card className={cn("flex w-72 p-4 shadow-md", bgColor)}>
+    <Card className={cn("flex w-72 border-none p-4 shadow-md", bgColor)}>
       <CardContent className="flex flex-row items-center gap-3">
-        <Icon className="text-white" size={70} />
+        <Icon className="h-20 w-20 text-white" />
         <h3 className="text-2xl text-white">{title}</h3>
       </CardContent>
     </Card>
