024b69381e1984f38c63e1a08e49366047a62f90
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-19 22:15:55 -0700

    feat:checkbox-btn/common

---
+++ src/components/common/checkbox-btn.tsx
@@ -0,0 +1,25 @@
+
+import { Checkbox } from "@/components/ui/checkbox"
+import { cn } from "@/lib/utils"
+import { Label } from "@radix-ui/react-label"
+
+type CheckboxBtnProps={
+    id:string;
+    className:string;
+    label:string;
+}
+
+const Checkboxtn = ({id,className,label}:CheckboxBtnProps)=> {
+  return (
+  <>
+      <Checkbox id={id} />
+      <Label
+        htmlFor={id}
+        className={cn("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70", className)}
+      >
+      {label}
+      </Label>
+  </>
+  )
+}
+export default Checkboxtn
