e5e42703b29873966d76cad9726b043b557f8e61
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-10-20 17:05:07 +0330

    refactor: title-box

--- src/components/lobby/title-box.tsx
+++ src/components/lobby/title-box.tsx
@@ -22,9 +22,9 @@ type TitleBoxProps = {
 
 const TitleBox = ({ lang, title, desc, className }: TitleBoxProps) => {
   return (
-    <Card className={cn(`w-fit rounded-xl px-12 py-2 shadow`, className)}>
-      <CardContent className="flex flex-col items-center justify-center text-purple-primary">
-        <CardTitle className="text-3xl font-bold">{title[lang]}</CardTitle>
+    <Card className={cn(`rounded-xl px-12 py-3 shadow`, className)}>
+      <CardContent className="flex flex-col items-center justify-center gap-1.5 text-purple-primary">
+        <CardTitle className="text-3xl font-black">{title[lang]}</CardTitle>
         {desc && (
           <CardDescription className="text-center text-lg">
             {desc[lang]}
