550c1ccd13f08365b91c5a4fcccbc21035c2cbb8
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-19 23:09:07 -0700

    feat:pagination/common

---
+++ src/components/common/pagination.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+const Pagination = () => {
+  return (
+    <nav>
+      <ul className="flex flex-row-reverse items-center gap-1">
+        <li>
+          <Link
+            href="#"
+            className="rounded-full border hover:border-purple-primary "
+          >
+            <ChevronLeft />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="#"
+            className="rounded-full  border px-2  hover:border-purple-primary focus:bg-purple-primary "
+          >
+            1
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="#"
+            className="rounded-full  border px-2  hover:border-purple-primary focus:bg-purple-primary "
+          >
+            1
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="#"
+            className="rounded-full  border px-2  hover:border-purple-primary focus:bg-purple-primary "
+          >
+            1
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="#"
+            className="rounded-full border hover:border-purple-primary"
+          >
+            <ChevronRight />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Pagination;
