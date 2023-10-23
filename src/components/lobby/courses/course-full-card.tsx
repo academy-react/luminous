263767028d88407564a466a52d55a3384b901f92
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-23 14:42:48 -0700

    refactor: course-full-card responsive

--- src/components/lobby/courses/course-full-card.tsx
+++ src/components/lobby/courses/course-full-card.tsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 import { type Locale } from "#/i18n.config";
 
@@ -11,39 +17,50 @@ type CourseCardProps = {
   title: { [key in Locale]: string };
   desc: { [key in Locale]: string };
   teacher: { [key in Locale]: string };
+  image: string;
 };
 
-const CourseFullCard = ({ lang, title, desc, teacher }: CourseCardProps) => {
+const CourseFullCard = ({
+  lang,
+  title,
+  desc,
+  teacher,
+  image,
+}: CourseCardProps) => {
   return (
     <Card className="h-full w-full rounded-xl border p-4 shadow">
-      <CardContent className="flex h-full w-full flex-col justify-between">
-        <Link href="#" className="relative aspect-video w-full">
+      <CardContent className="flex h-full w-full flex-col items-center gap-3 sm:flex-row lg:flex-col lg:justify-between">
+        <Link href="#" className="relative aspect-video  w-full">
           <Image
-            src="/images/dev/node-js.jpg"
+            src={image}
             alt="course-picture"
             fill
             className="rounded-xl object-contain"
           />
         </Link>
-        <div className="flex w-full items-center justify-between">
-          <CardTitle className="text-2xl font-bold">{title[lang]}</CardTitle>
-          <h4 className="text-lg text-gray-600">{teacher[lang]}</h4>
-        </div>
-        <div className="flex w-full items-end justify-between">
-          <p className="line-clamp-2 pe-5 text-base text-gray-600">
-            {desc[lang]}
-          </p>
-          <Button className="w-full max-w-fit rounded-lg border-2 border-purple-primary p-3 text-purple-primary duration-300 hover:bg-purple-primary hover:text-white">
-            <Link href="#" className="">
-              {
+        <CardFooter>
+          <div className="mb-2 flex w-full items-center justify-between lg:mb-4">
+            <CardTitle className="text-xl font-bold lg:text-2xl">
+              {title[lang]}
+            </CardTitle>
+            <h4 className="text-lg text-gray-600">{teacher[lang]}</h4>
+          </div>
+          <div className="hidden w-full items-end justify-between sm:flex">
+            <p className="line-clamp-2 pe-5 text-base text-gray-600">
+              {desc[lang]}
+            </p>
+            <Button className="w-full max-w-fit rounded-lg border-2 border-purple-primary p-3 text-purple-primary duration-300 hover:bg-purple-primary hover:text-white">
+              <Link href="#" className="">
                 {
-                  fa: "مشاهده دوره",
-                  en: "View Course",
-                }[lang]
-              }
-            </Link>
-          </Button>
-        </div>
+                  {
+                    fa: "مشاهده دوره",
+                    en: "View Course",
+                  }[lang]
+                }
+              </Link>
+            </Button>
+          </div>
+        </CardFooter>
       </CardContent>
     </Card>
   );
