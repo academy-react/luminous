9f24d243265afcb3bdbad57b50fc0845b2b34dfa
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-23 06:23:37 -0700

    feat: create course-blog-main-layout

---
+++ src/components/course-page/course-blog-main-layout.tsx
@@ -0,0 +1,20 @@
+
+// type MainLayoutProps = {
+//     sidebar:
+// }
+
+const MainLayout = () => {
+  return (
+    <div className="flex h-[700px] w-full border border-black ">
+      <div className="w-[30%] border border-black">
+        <div className="w-full rounded-xl p-4 shadow">search</div>
+        <div className="w-full rounded-xl p-4 shadow">sidebar</div>
+      </div>
+      <div className="w-[70%] border border-black">
+        <div className="w-full rounded-xl p-4 shadow">topMenu</div>
+        <div className="w-full rounded-xl p-4 shadow">list</div>
+      </div>
+    </div>
+  );
+};
+export default MainLayout;
