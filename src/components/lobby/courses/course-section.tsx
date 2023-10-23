a755a6f3ae891e1b0854dde64797ff165521fc3b
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-23 14:41:40 -0700

    refactor: course-section

--- src/components/lobby/courses/course-section.tsx
+++ src/components/lobby/courses/course-section.tsx
@@ -4,37 +4,39 @@ import { courses } from "@/dict/lobby/courses.dict";
 import { coursesTitleBox } from "@/dict/lobby/lobby-title-box.dict";
 
 import { Button } from "@/components/ui/button";
-import CourseCard from "@/components/lobby/courses/course-card";
-import SideCard from "@/components/lobby/courses/course-side-card";
+import CourseFullCard from "@/components/lobby/courses/course-full-card";
+import CourseMiniCard from "@/components/lobby/courses/course-mini-card";
 import TitleBox from "@/components/lobby/title-box";
 
 import { type Locale } from "#/i18n.config";
 
 const CourseSection = ({ lang }: { lang: Locale }) => {
   return (
-    <div className="flex w-full flex-col items-center gap-10">
+    <section className="container flex w-full flex-col items-center gap-10">
       <TitleBox
         lang={lang}
         title={coursesTitleBox.title}
         desc={coursesTitleBox.subtitle}
       />
-      <div className="container flex w-full max-w-screen-lg gap-4">
-        <div className="w-[70%]">
+      <div className="container flex w-full max-w-screen-lg flex-col gap-4 lg:flex-row">
+        <div className="w-full lg:w-[70%]">
           {courses[0] && (
-            <CourseCard
+            <CourseFullCard
               lang={lang}
               title={courses[0].title}
               desc={courses[0].description}
               teacher={courses[0].teacher}
+              image={courses[0].image}
             />
           )}
         </div>
-        <div className="w-[30%] rounded-xl border shadow">
-          <div className="flex h-full w-full flex-col items-center justify-center gap-4 p-4">
-            <SideCard />
-            <SideCard />
-            <SideCard />
-            <Button className="w-full max-w-fit rounded-lg border-2 border-purple-primary p-3 text-purple-primary duration-300 hover:bg-purple-primary hover:text-white">
+        <div className="w-full rounded-xl border shadow lg:w-[30%]">
+          <div className="flex h-full w-full flex-row items-center justify-center gap-4 p-4 lg:flex-col">
+            {courses.slice(1, 4).map((course) => {
+              return <CourseMiniCard key={course.id} image={course.image} />;
+            })}
+
+            <Button className="hidden w-full max-w-fit rounded-lg border-2 border-purple-primary p-3 text-purple-primary duration-300 hover:bg-purple-primary hover:text-white sm:block">
               <Link href="#" className="">
                 {
                   {
@@ -47,7 +49,7 @@ const CourseSection = ({ lang }: { lang: Locale }) => {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
