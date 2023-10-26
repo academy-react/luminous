1d8d60c9dabe5382ccc39b4e0414a23da37f0b8d
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-26 08:40:54 -0700

    refactor: add courseList

--- src/app/[lang]/(lobby)/courses/page.tsx
+++ src/app/[lang]/(lobby)/courses/page.tsx
@@ -1,10 +1,11 @@
 
 import { type Locale } from "#/i18n.config";
+import { CourseList } from "@/components/course-page/course-list";
 
 const CoursesPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
   return (
     <main className="flex h-full items-center justify-center px-36">
-  
+  <CourseList lang={lang}/>
     </main>
   );
 };
