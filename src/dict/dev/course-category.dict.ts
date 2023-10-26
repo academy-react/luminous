84baa1120a3fd81e67a2eab87515607364853bf0
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-25 22:57:17 -0700

    refactor: change id type

--- src/dict/dev/course-category.dict.ts
+++ src/dict/dev/course-category.dict.ts
@@ -2,46 +2,46 @@ import {type Locale } from "#/i18n.config";
 
 export interface CourseCategoryProps{
     title:{[key in Locale]:string;}
-    id:number;
+    id:string;
   }
   export const CourseCategory:CourseCategoryProps[]=[
     {
-      id:1,
+      id:"r1",
       title:{
         fa:"فرانت اند",
         en:"front end",
       }
     },
     {
-      id:2,
+      id:"r2",
       title:{
         fa:"بک اند",
         en:"back end",
       }
     },
     {
-      id:3,
+      id:"r3",
       title:{
         fa:"هوش مصنوعی",
         en:"Artificial intelligence",
       }
     },
     {
-      id:4,
+      id:"r4",
       title:{
         fa:"دیتابیس",
         en:"database",
       }
     },
     {
-      id:5,
+      id:"r5",
       title:{
         fa:"امنیت",
         en:"security",
       }
     }, 
      {
-      id:6,
+      id:"r6",
       title:{
         fa:"تولید محتوا",
         en:"Content production",
