1a967bdd93683427c56a827a5692f031c36fcf7d
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-26 13:59:20 -0700

    refactor: change px from 32 to 20

--- src/app/[lang]/(lobby)/courses/page.tsx
+++ src/app/[lang]/(lobby)/courses/page.tsx
@@ -1,11 +1,11 @@
+import { CourseList } from "@/components/course-page/course-list";
 
 import { type Locale } from "#/i18n.config";
-import { CourseList } from "@/components/course-page/course-list";
 
 const CoursesPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
   return (
-    <main className="flex h-full items-center justify-center px-36">
-  <CourseList lang={lang}/>
+    <main className="mt-10 flex h-full items-center justify-center px-20">
+      <CourseList lang={lang} />
     </main>
   );
 };
