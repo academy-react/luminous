007a91dd1cf25c6e97914a01c103906712180e97
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-26 13:57:21 -0700

    refactor: change hight

--- src/components/course-page/course-list.tsx
+++ src/components/course-page/course-list.tsx
@@ -1,18 +1,18 @@
-
-import { type Locale } from "#/i18n.config";
 import { courses } from "@/dict/lobby/courses.dict";
 
+import { type Locale } from "#/i18n.config";
+
+import { CourseFullCard } from "./course-full-card";
 import { FilterByCategory } from "./sort-filter/filter-by-category";
 import { FilterByTeacher } from "./sort-filter/fiter-by-teacher";
 import { Search } from "./sort-filter/search-in-courses";
 import { SortBySize } from "./sort-filter/sort-by-size";
 import { SortByTitle } from "./sort-filter/sort-by-title";
-import { CourseFullCard } from "./course-full-card";
 
 export const CourseList = ({ lang }: { lang: Locale }) => {
   return (
-    <div className="flex h-[700px] w-full gap-3 border border-black">
-      <div className="flex w-[25%] flex-col gap-3 border border-black">
+    <div className="flex h-full w-full gap-3 ">
+      <div className="flex w-[25%] flex-col gap-3 ">
         <div className="w-full rounded-xl p-4 shadow">
           <Search />
         </div>
@@ -23,7 +23,7 @@ export const CourseList = ({ lang }: { lang: Locale }) => {
           <FilterByTeacher />
         </div>
       </div>
-      <div className="flex w-[75%] flex-col  gap-3 border border-black">
+      <div className="flex w-[75%] flex-col  gap-3">
         <div className="flex items-center justify-between rounded-xl p-4 shadow">
           <div className="w-full">
             <SortByTitle />
@@ -33,15 +33,9 @@ export const CourseList = ({ lang }: { lang: Locale }) => {
           </div>
         </div>
 
-        <div className="w-full rounded-xl p-4 shadow">
-        {courses.map((item) => {
-            return (
-              <CourseFullCard
-                key={item.id}
-                item={item} 
-                lang={lang}
-              />
-            );
+        <div className="flex w-full flex-col  gap-4 rounded-xl shadow">
+          {courses.map((item) => {
+            return <CourseFullCard key={item.id} item={item} lang={lang} />;
           })}
         </div>
       </div>
