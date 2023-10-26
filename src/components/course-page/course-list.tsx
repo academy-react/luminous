d92b6ef41fd68c9f7522d376c3059e533f2d05ae
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-26 01:14:11 -0700

    refactor: map items by category

--- src/components/course-page/course-list.tsx
+++ src/components/course-page/course-list.tsx
@@ -1,16 +1,24 @@
+import { type Locale } from "#/i18n.config";
+
 import { FilterByCategory } from "./sort-filter/filter-by-category";
+import { FilterByTeacher } from "./sort-filter/fiter-by-teacher";
+import { Search } from "./sort-filter/search-in-courses";
+import { SortByTitle } from "./sort-filter/sort-by-title";
 
-export const CourseList = () =>{
-    return(
-
-
-    <div className="flex h-[700px] w-full border border-black ">
-      <div className="w-[30%] border border-black">
-        <div className="w-full rounded-xl p-4 shadow">search</div>
-        <div className="w-full rounded-xl p-4 shadow"><FilterByCategory/></div>
+export const CourseList = ({ lang }: { lang: Locale }) => {
+  return (
+    <div className="flex h-[700px] w-full gap-3 border border-black">
+      <div className="flex w-[30%] flex-col gap-3 border border-black">
+        <div className="w-full rounded-xl p-4 shadow"><Search/></div>
+        <div className="w-full rounded-xl p-4 shadow">
+          <FilterByCategory lang={lang} />
+        </div>
+        <div className="w-full rounded-xl p-4 shadow">
+          <FilterByTeacher />
+        </div>
       </div>
       <div className="w-[70%] border border-black">
-        <div className="w-full rounded-xl p-4 shadow">topMenu</div>
+        <div className="w-full rounded-xl p-4 shadow"><SortByTitle/></div>
         <div className="w-full rounded-xl p-4 shadow">list</div>
       </div>
     </div>
