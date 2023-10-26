9755fcda71828526c6a40b5c5a7612dc7bc271c0
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-25 22:45:17 -0700

    refactor: modified some changes

--- src/components/course-page/course-list.tsx
+++ src/components/course-page/course-list.tsx
@@ -1,3 +1,18 @@
+import { FilterByCategory } from "./sort-filter/filter-by-category";
+
 export const CourseList = () =>{
-    return(<></>)
-}
+    return(
+
+
+    <div className="flex h-[700px] w-full border border-black ">
+      <div className="w-[30%] border border-black">
+        <div className="w-full rounded-xl p-4 shadow">search</div>
+        <div className="w-full rounded-xl p-4 shadow"><FilterByCategory/></div>
+      </div>
+      <div className="w-[70%] border border-black">
+        <div className="w-full rounded-xl p-4 shadow">topMenu</div>
+        <div className="w-full rounded-xl p-4 shadow">list</div>
+      </div>
+    </div>
+  );
+};
