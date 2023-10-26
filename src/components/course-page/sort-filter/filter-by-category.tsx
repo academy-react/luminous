36983d660c9e0aaea2bdbc41481dd7277bd5d86d
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-26 13:56:19 -0700

    refactor: text size and style

--- src/components/course-page/sort-filter/filter-by-category.tsx
+++ src/components/course-page/sort-filter/filter-by-category.tsx
@@ -9,14 +9,14 @@ import { type Locale } from "#/i18n.config";
 
 export const FilterByCategory = ({ lang }: { lang: Locale }) => {
   return (
-    <CheckboxGroup className="flex w-full flex-col justify-start gap-4 text-base font-bold">
+    <CheckboxGroup className="flex w-full flex-col justify-start gap-4 text-base font-bold text-[#333]">
       دسته بندی دوره ها
       {courseCategory.map((item) => (
         <Checkboxbtn
           key={item.id}
           id={item.id}
           title={item.title[lang]}
-          className="flex flex-row  text-sm"
+          className="flex flex-row  font-[#666] text-sm"
         />
       ))}
     </CheckboxGroup>
