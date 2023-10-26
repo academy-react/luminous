4d781becbfedf7f0c930beab26f19589600c9c97
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-26 08:08:11 -0700

    refactor: set icon...test

--- src/components/course-page/sort-filter/sort-by-size.tsx
+++ src/components/course-page/sort-filter/sort-by-size.tsx
@@ -1,22 +1,23 @@
 "use client"
 
-import { LayoutGrid, LayoutList } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Icons ,type Icon  } from "@/components/utils/icons";
+import { LayoutGrid, LayoutList } from "lucide-react";
 
 export const SortBySize = () => {
   return (
     <RadioGroup
       defaultValue="grid-layout"
-      className="flex flex-row-reverse rounded-xl px-4 py-2 shadow-lg"
+      className="flex flex-row-reverse "
     >
       <div className={cn("flex items-center space-x-2")}>
         <RadioGroupItem value="grid-layout" id="r1" />
         <Label htmlFor="r1">
-          <LayoutGrid />
+          <LayoutGrid/>
         </Label>
       </div>
       <div className={cn("flex appearance-none items-center space-x-2")}>
