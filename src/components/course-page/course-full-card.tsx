e2c6f6679974a1061b86d1cbd0b53b306b7baf17
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-26 08:40:23 -0700

    refactor: made it object

--- src/components/course-page/course-full-card.tsx
+++ src/components/course-page/course-full-card.tsx
@@ -1,24 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export const CourseFullCard = () => {
+import {type Locale } from "#/i18n.config";
+
+type CourseFullCardProps = {
+  image: string;
+  title: { [key in Locale]: string };
+  description: { [key in Locale]: string };
+  teacher: { [key in Locale]: string };
+};
+export const CourseFullCard = (
+  { image, title, description, teacher }: CourseFullCardProps,
+  { lang }: { lang: Locale }
+) => {
   return (
     <div className="flex w-full  gap-4 rounded-xl p-4 shadow-lg">
       <Link href="#" className="relative aspect-[2/1] basis-1/3 ">
-        <Image
-          src="/images/node jd.svg"
-          alt=""
-          fill
-          className="rounded-xl object-cover"
-        />
+        <Image src={image} alt="" fill className="rounded-xl object-cover" />
       </Link>
       <div className=" basis-2/3">
         <div className="flex justify-between">
           <div className="flex flex-col gap-2">
-            <h2 className="text-2xl font-bold">نود جی اس:دوره کامل</h2>
+            <h2 className="text-2xl font-bold">{title[lang]}</h2>
             <p className="w-[70%] text-base text-[#707070]">
-              متون کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذش سوالات
-              پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+              {description[lang]}
             </p>
           </div>
           <ul className="flex flex-col  rounded-xl px-2 shadow-lg">
@@ -29,7 +34,7 @@ export const CourseFullCard = () => {
         </div>
         <div className="flex items-end  justify-between">
           <ul className="flex gap-4">
-            <li>مدرس:استاد بحرالعلوم</li>
+            <li>مدرس:{teacher[lang]}</li>
             <li>مدت زمان دوره:80ساعت</li>
             <li>*****</li>
           </ul>
