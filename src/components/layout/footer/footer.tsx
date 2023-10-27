d08d0dbfb5b5c2bf021ece68071053137ad99922
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-27 07:13:52 -0700

    refactor: change hieght of footer

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -12,7 +12,7 @@ import { type Locale } from "#/i18n.config";
 
 const Footer = ({ lang }: { lang: Locale }) => {
   return (
-    <>
+    <div className="mt-28">
       {/* footer background */}
       <div className="relative aspect-[7/1] w-full">
         <Image
@@ -23,7 +23,7 @@ const Footer = ({ lang }: { lang: Locale }) => {
         />
       </div>
       {/* Footer Section */}
-      <footer className="grid w-full grid-rows-3 place-content-center py-16 lg:grid-cols-3 lg:grid-rows-1">
+      <footer className="grid w-full grid-rows-3 place-content-center lg:grid-cols-3 lg:grid-rows-1">
         {/* Info Area */}
         <div className="flex items-start justify-center gap-6">
           {/* Nav Bar */}
@@ -104,7 +104,7 @@ const Footer = ({ lang }: { lang: Locale }) => {
           </div>
         </div>
       </footer>
-    </>
+    </یهر>
   );
 };
 
