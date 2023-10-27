d4bf0fd6c5f7b770c25ac830c9a45e8495fb749f
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-27 13:44:57 -0700

    fix: rename component name

--- src/app/[lang]/(lobby)/courses/page.tsx
+++ src/app/[lang]/(lobby)/courses/page.tsx
@@ -1,11 +1,11 @@
-import { CourseList } from "@/components/course-page/course-list";
 
 import { type Locale } from "#/i18n.config";
+import CourseSection from "@/components/lobby/courses/course-section";
 
 const CoursesPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
   return (
     <main className="mt-10 flex h-full items-center justify-center px-20">
-      <CourseList lang={lang} />
+      <CourseSection lang={lang} />
     </main>
   );
 };
