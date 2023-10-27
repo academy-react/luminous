89a239d62d9e99fd43212e47fa38086c1f15486f
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-27 06:53:01 -0700

    refactor: change gap and basis

--- src/components/course-page/course-full-card.tsx
+++ src/components/course-page/course-full-card.tsx
@@ -17,11 +17,11 @@ export const CourseFullCard = ({
   lang,
 }: CourseFullCardProps) => {
   return (
-    <div className="flex h-[160px] w-full gap-4 rounded-xl px-4 py-2 shadow-lg">
-      <Link href="#" className="relative aspect-[2/1] basis-1/4 ">
+    <div className="flex h-[160px] w-full gap-3 rounded-xl px-4 py-3 shadow">
+      <Link href="#" className="relative aspect-[2/1] basis-1/3 ">
         <Image src={image} alt="" fill className="rounded-xl object-cover" />
       </Link>
-      <div className=" basis-3/4">
+      <div className=" basis-2/3">
         <div className="flex justify-between">
           <div className="flex flex-col gap-2">
             <h2 className="text-xl font-bold text-[#333]">{title[lang]}</h2>
