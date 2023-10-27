434362fe31a4685557b023a212778f7f85ca3af0
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-27 06:52:00 -0700

    refactor: add parent

--- src/components/course-page/course-list.tsx
+++ src/components/course-page/course-list.tsx
@@ -1,15 +1,18 @@
-
 import { type Locale } from "#/i18n.config";
+
 import { CourseContent } from "./course-content";
 import { CourseSideBar } from "./course-side-bar";
-
-
 
 export const CourseList = ({ lang }: { lang: Locale }) => {
   return (
     <div className="flex h-full w-full gap-3 ">
-      <CourseSideBar lang={lang} />
-      <CourseContent lang={lang} />
+      <div className="flex w-[25%] flex-col  ">
+        <CourseSideBar lang={lang} />
+      </div>
+      <div className="flex w-[75%] flex-col ">
+        <CourseContent lang={lang} />
+      </div>
+
     </div>
   );
 };
