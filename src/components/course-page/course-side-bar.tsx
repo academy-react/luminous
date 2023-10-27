1745b8e919dc7907bbe13be1480b6155707da470
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-27 02:31:10 -0700

    feat: create course-side-bar

---
+++ src/components/course-page/course-side-bar.tsx
@@ -0,0 +1,21 @@
+import {type Locale } from "#/i18n.config"
+import { FilterByCategory } from "./sort-filter/filter-by-category"
+import { FilterByTeacher } from "./sort-filter/fiter-by-teacher"
+import { Search } from "./sort-filter/search-in-courses"
+
+export const CourseSideBar = ({lang}:{lang: Locale}) =>{
+    return(
+
+        <div className="flex w-[25%] flex-col gap-3 ">
+        <div className="w-full rounded-xl p-4 shadow">
+          <Search />
+        </div>
+        <div className="w-full rounded-xl p-4 shadow">
+          <FilterByCategory lang={lang} />
+        </div>
+        <div className="w-full rounded-xl p-4 shadow">
+          <FilterByTeacher />
+        </div>
+      </div>
+    )
+}
