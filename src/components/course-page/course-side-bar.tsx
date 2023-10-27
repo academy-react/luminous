d2122f2acea75f4d1aafc2bf0531df91e9f540db
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-27 06:50:08 -0700

    refactor: change width

--- src/components/course-page/course-side-bar.tsx
+++ src/components/course-page/course-side-bar.tsx
@@ -1,21 +1,21 @@
-import {type Locale } from "#/i18n.config"
-import { FilterByCategory } from "./sort-filter/filter-by-category"
-import { FilterByTeacher } from "./sort-filter/fiter-by-teacher"
-import { Search } from "./sort-filter/search-in-courses"
+import { type Locale } from "#/i18n.config";
 
-export const CourseSideBar = ({lang}:{lang: Locale}) =>{
-    return(
+import { FilterByCategory } from "./sort-filter/filter-by-category";
+import { FilterByTeacher } from "./sort-filter/fiter-by-teacher";
+import { Search } from "./sort-filter/search-in-courses";
 
-        <div className="flex w-[25%] flex-col gap-3 ">
-        <div className="w-full rounded-xl p-4 shadow">
-          <Search />
-        </div>
-        <div className="w-full rounded-xl p-4 shadow">
-          <FilterByCategory lang={lang} />
-        </div>
-        <div className="w-full rounded-xl p-4 shadow">
-          <FilterByTeacher />
-        </div>
+export const CourseSideBar = ({ lang }: { lang: Locale }) => {
+  return (
+    <div className="flex flex-col gap-3"> 
+      <div className="w-full rounded-xl p-4 shadow">
+        <Search />
       </div>
-    )
-}
+      <div className="w-full rounded-xl p-4 shadow">
+        <FilterByCategory lang={lang} />
+      </div>
+      <div className="w-full rounded-xl p-4 shadow">
+        <FilterByTeacher />
+      </div>
+    </div>
+  );
+};
