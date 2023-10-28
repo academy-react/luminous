ca680b7dbad7c2d6cb4612b5716b1dd4039eee46
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-28 06:30:28 -0700

    refactor: px-20 to px-28

--- src/app/[lang]/(lobby)/courses/page.tsx
+++ src/app/[lang]/(lobby)/courses/page.tsx
@@ -4,7 +4,7 @@ import { CoursePageSection } from "@/components/course-page/course-page-section"
 
 const CoursesPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
   return (
-    <main className="mt-10 flex h-full items-center justify-center px-20">
+    <main className="mt-10 flex h-full items-center justify-center px-28">
       <CoursePageSection lang={lang} />
     </main>
   );
