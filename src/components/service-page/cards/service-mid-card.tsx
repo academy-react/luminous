9c7b0d735d7e398b5788b53f46b5d8a7bcb3361d
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-27 22:43:58 -0700

    feat: create mid card

---
+++ src/components/service-page/cards/service-mid-card.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { type Locale } from "#/i18n.config";
+
+type ServiceMidCardProps = {
+  item: {
+    image: string;
+    title: { [key in Locale]: string };
+    description: { [key in Locale]: string };
+    category: { [key in Locale]: string };
+  };
+  lang: Locale;
+};
+export const ServiceMidCard = ({
+  item: { image, title, description, category },
+  lang,
+}: ServiceMidCardProps) => {
+  return (
+    <div className="flex w-full gap-3 rounded-xl px-4 py-3 shadow">
+      <Link href="#" className="relative aspect-[3/2]">
+        <Image src={image} alt="" fill className="rounded-xl object-cover" />
+      </Link>
+      <div className="">
+        <div className="flex flex-col items-start gap-2">
+          <h2 className="text-xl font-bold text-[#333]">{title[lang]}</h2>
+          <p className="line-clamp-2 w-[80%] text-sm text-[#666] ">
+            {description[lang]}
+          </p>
+        </div>
+        <div className="mt-10 flex items-end justify-between">
+          <span className="rounded-xl border-2 border-[#58AD57] px-1 text-sm text-[#58AD57]">
+            {category[lang]}
+          </span>
+          <Link
+            href="#"
+            className="border-purple-primary text-sm text-purple-primary"
+          >
+            {lang === "fa" ? "ادامه مطلب" : "more"}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
