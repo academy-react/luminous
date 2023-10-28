68ad399a1d74ccba35389350864d27ac612465be
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-28 06:23:51 -0700

    refactor: edit style

--- src/components/blog-page/cards/blog-mid-card.tsx
+++ src/components/blog-page/cards/blog-mid-card.tsx
@@ -17,28 +17,24 @@ export const BlogMidCard = ({
   lang,
 }: BlogMidCardProps) => {
   return (
-    <div className="flex w-full gap-3 rounded-xl px-4 py-3 shadow">
+    <div className="flex w-full flex-col gap-2 rounded-xl px-4 py-2 shadow">
       <Link href="#" className="relative aspect-[3/2]">
         <Image src={image} alt="" fill className="rounded-xl object-cover" />
       </Link>
-      <div className="">
-        <div className="flex flex-col items-start gap-2">
-          <h2 className="text-xl font-bold text-[#333]">{title[lang]}</h2>
-          <p className="line-clamp-2 w-[80%] text-sm text-[#666] ">
-            {description[lang]}
-          </p>
-        </div>
-        <div className="mt-10 flex items-end justify-between">
-          <span className="rounded-xl border-2 border-[#58AD57] px-1 text-sm text-[#58AD57]">
-            {category[lang]}
-          </span>
-          <Link
-            href="#"
-            className="border-purple-primary text-sm text-purple-primary"
-          >
-            {lang === "fa" ? "ادامه مطلب" : "more"}
-          </Link>
-        </div>
+      <div className="flex flex-col items-start gap-2">
+        <h2 className="text-xl font-bold text-[#333]">{title[lang]}</h2>
+        <p className="line-clamp-2 w-[80%] text-sm text-[#666] ">
+          {description[lang]}
+        </p>
+      </div>
+      <div className="mt-8 flex items-end justify-between">
+        <span className="text-sm text-[#5555]">#{category[lang]}</span>
+        <Link
+          href="#"
+          className="rounded-xl border border-purple-primary p-1 text-sm text-purple-primary"
+        >
+          {lang === "fa" ? "ادامه مطلب" : "more"}
+        </Link>
       </div>
     </div>
   );
