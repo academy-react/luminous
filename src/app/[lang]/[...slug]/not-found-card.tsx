0d804ef42f7f458f3c206d92ebd79905b6277825
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-10-29 06:51:06 +0330

    refactor: 404 structure

--- src/app/[lang]/[...slug]/not-found-card.tsx
+++ src/app/[lang]/[...slug]/not-found-card.tsx
@@ -6,8 +6,8 @@ import { type Locale } from "#/i18n.config";
 const NotFoundCard = ({ lang }: { lang: Locale }) => {
   return (
     <>
-      <div className="mx-auto  flex w-1/3 flex-col items-center  ">
-        <div className="relative aspect-[2/1] w-full ">
+      <div className="mx-auto flex w-1/3 flex-col items-center">
+        <div className="relative aspect-[2/1] w-full">
           <Image
             alt="404-bg"
             src="/images/artworks/404.svg"
@@ -17,7 +17,7 @@ const NotFoundCard = ({ lang }: { lang: Locale }) => {
         </div>
         <Link
           href={`/${lang}`}
-          className=" rounded-xl border-2 bg-[#A644C1] px-4 py-1 text-base text-white"
+          className="rounded-xl border-2 bg-[#A644C1] px-4 py-1 text-base text-white"
         >
           بازگشت به صفحه اصلی
         </Link>
