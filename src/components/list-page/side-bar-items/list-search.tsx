ab88c426d18667b5487ede9ba1fb49a2cd63b19f
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-30 01:10:17 -0700

    refactor: style

--- src/components/list-page/side-bar-items/list-search.tsx
+++ src/components/list-page/side-bar-items/list-search.tsx
@@ -1,9 +1,21 @@
 import { cn } from "@/lib/utils";
 
-export const ListSearch = ({ className }: { className?: string }) => {
+import { type Locale } from "#/i18n.config";
+
+export const ListSearch = ({
+  className,
+  lang,
+}: {
+  className?: string;
+  lang: Locale;
+}) => {
   return (
     <div className={cn("w-full rounded-xl p-4 shadow", className)}>
-    <input type="text"  className="border-none focus:outline-none" placeholder="جستجو..."/>
-  </div>
+      <input
+        type="text"
+        className="border-none placeholder-[#444] focus:placeholder-transparent focus:outline-none text-base"
+        placeholder={lang === "fa" ? "جستجو..." : "search..."}
+      />
+    </div>
   );
 };
