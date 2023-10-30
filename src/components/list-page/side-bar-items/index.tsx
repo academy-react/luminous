b3a4d89e43bc01f73ba58df50ae3ced897dd0348
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-30 01:06:34 -0700

    feat: create index for multi exports

---
+++ src/components/list-page/side-bar-items/index.tsx
@@ -0,0 +1,16 @@
+
+import {ListCategory} from "./list-category";
+export {ListCategory};
+
+import { ListFree } from "./list-free";
+export {ListFree};
+
+import { ListSearch } from "./list-search";
+export {ListSearch};
+
+import { ListTeacher } from "./list-teacher";
+export {ListTeacher};
+
+import { ListCommingSoon } from "./list-comming-soon";
+export {ListCommingSoon};
+
