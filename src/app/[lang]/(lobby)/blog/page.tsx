691f40f4e21e3cc5fc8a8f238a88e84368054140
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-10-31 04:39:26 -0700

    refactor: blog page

--- src/app/[lang]/(lobby)/blog/page.tsx
+++ src/app/[lang]/(lobby)/blog/page.tsx
@@ -1,11 +1,47 @@
-import { BlogPageSection } from "@/components/blog-page/blog-page-section";
+import { blogs } from "@/dict/dev/blog-list.dict";
+import { blogCategory } from "@/dict/list/list-category.dict";
+import { blogSortOptions } from "@/dict/list/list-options.dict";
+
+import {
+  ListContent,
+  ListPage,
+  ListSideBar,
+  ListTitle,
+} from "@/components/list-page/list-page";
+import {
+  ListCategory,
+  ListCommingSoon,
+  ListFree,
+  ListSearch,
+  ListTeacher,
+} from "@/components/list-page/side-bar-items";
 
 import { type Locale } from "#/i18n.config";
+import { BlogFullCard } from "@/components/cards/blog-full-card";
+import { BlogMidCard } from "@/components/cards/blog-mid-card";
 
 const BlogPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
   return (
-    <main className="mt-10 flex h-full items-center justify-center px-28">
-      <BlogPageSection lang={lang} />
+    <main className="mt-10 h-full w-full">
+      <ListPage>
+        <ListTitle />
+        <ListSideBar>
+          <ListSearch lang={lang} />
+          <ListCategory category={blogCategory} lang={lang} />
+          <div className="flex gap-3 md:flex-col">
+            <ListFree lang={lang} />
+            <ListCommingSoon lang={lang} />
+          </div>
+          <ListTeacher lang={lang} />
+        </ListSideBar>
+        <ListContent
+          sortOptions={blogSortOptions}
+          lang={lang}
+          FullCard={BlogFullCard}
+          MidCard={BlogMidCard}
+          data={blogs}
+        />
+      </ListPage>
     </main>
   );
 };
