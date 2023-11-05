3fd8622ce23b4f1a61bcfb529d391e6c9e53f999
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-05 14:28:21 +0330

    feat: comments section

--- src/components/pages/details-page/content/comment-section.tsx
+++ src/components/pages/details-page/content/comment-section.tsx
@@ -1,3 +1,28 @@
-export const CommentSection = () => {
-  return <div className="h-screen rounded-xl bg-card"></div>;
+import { Button, H3 } from "@/components/elements/ui";
+
+import { commentsDict } from "@/dict/dev/comments.dict";
+
+import { type Locale } from "#/i18n.config";
+
+import { Comment } from "./comment/comment";
+
+export const CommentSection = ({ lang }: { lang: Locale }) => {
+  return (
+    <section className="rounded-xl bg-card px-7 py-6">
+      <div className="mb-5 flex items-center justify-between">
+        <div className="flex items-center gap-x-3.5">
+          <span className="h-10 w-2.5 rounded-sm bg-pink-500" />
+          <H3 className="text-3xl font-bold">نظرات</H3>
+        </div>
+        <Button className="border-[3px] border-green-500 bg-green-500 text-white transition-colors duration-500 hover:bg-transparent hover:text-green-500">
+          ایجاد نظر جدید
+        </Button>
+      </div>
+      <section className="space-y-5">
+        {commentsDict.map((comment, index) => (
+          <Comment lang={lang} key={index} comment={comment} />
+        ))}
+      </section>
+    </section>
+  );
 };
---
+++ src/components/pages/details-page/content/comment/comment.tsx
@@ -0,0 +1,91 @@
+import Image from "next/image";
+
+import { Button } from "@/components/elements/ui";
+
+import { type CommentDictProps } from "@/dict/dev/comments.dict";
+
+import { type Locale } from "#/i18n.config";
+
+type CommentProps = {
+  lang: Locale;
+  comment: CommentDictProps;
+};
+
+export const Comment = ({ lang, comment }: CommentProps) => {
+  return (
+    <div className="rounded-2xl bg-gray-100 p-5">
+      <div className="flex items-start gap-x-5">
+        <UserAvatar
+          avatar={comment.sender.avatar}
+          userType={comment.sender.userType}
+        />
+        <div className="w-full">
+          <UserInfo name={comment.sender.name[lang]} date={comment.date} />
+          <CommentText text={comment.text[lang]} />
+          <section className="mt-7 space-y-4">
+            {comment.replies?.map((reply, index) => (
+              <Reply key={index} lang={lang} reply={reply} />
+            ))}
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const UserAvatar = ({
+  avatar,
+  userType,
+}: {
+  avatar: string;
+  userType: string;
+}) => {
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <div className="relative h-16 w-16">
+        <Image
+          src={avatar}
+          fill
+          alt="user-avatar"
+          className="rounded-full object-cover"
+        />
+      </div>
+      <span className="rounded-md bg-green-500 px-1.5 py-0.5 text-center text-xs text-white">
+        {userType}
+      </span>
+    </div>
+  );
+};
+
+const UserInfo = ({ name, date }: { name: string; date: string }) => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex flex-col gap-1">
+        <span className="text-xl font-medium">{name}</span>
+        <span className="text-xs">{date}</span>
+      </div>
+      <Button>reply</Button>
+    </div>
+  );
+};
+
+const CommentText = ({ text }: { text: string }) => {
+  return <p className="mt-3.5">{text}</p>;
+};
+
+const Reply = ({ lang, reply }: { lang: Locale; reply: CommentDictProps }) => {
+  return (
+    <div className="rounded-2xl bg-gray-200 p-5">
+      <div className="flex items-start gap-x-5">
+        <UserAvatar
+          avatar={reply.sender.avatar}
+          userType={reply.sender.userType}
+        />
+        <div className="w-full">
+          <UserInfo name={reply.sender.name[lang]} date={reply.date} />
+          <CommentText text={reply.text[lang]} />
+        </div>
+      </div>
+    </div>
+  );
+};
