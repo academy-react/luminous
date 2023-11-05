684c3cce61d7acb2df5a6bb7075d7ed8663dfd3f
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-05 03:03:31 -0800

    feat: user-panel dict

---
+++ src/dict/pages/user-panel.dict.ts
@@ -0,0 +1,42 @@
+import {type Locale } from "#/i18n.config";
+import { Icons, type Icon } from "@/components/assets/icons";
+
+export interface PanelAsideOptionProps{
+    id: number;
+    Icon: Icon;
+    title: {[lang in Locale]:string};
+}
+export const panelAsideOption:PanelAsideOptionProps[]=[
+    {
+    id: 1,
+    Icon: Icons.home,
+    title: {
+        fa: "پیشخوان",
+        en:"Dashboard"
+        },
+    },
+    {
+    id: 2,
+    Icon: Icons.myCourses,
+    title: {
+        fa: "دوره های من",
+        en:"My courses"
+        },
+    },
+    {
+    id: 3,
+    Icon: Icons.edit,
+    title: {
+        fa: "ویرایش پروفایل",
+        en:"Edit profile"
+        },
+    },
+    {
+    id: 4,
+    Icon: Icons.logOut,
+    title: {
+        fa: "خروج",
+        en:"Loug out"
+        },
+    },
+]
