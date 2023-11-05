682102c8d68e928ccd11632d0884a9917cd9be3e
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-04 22:57:58 -0700

    feat: create userPanel and layout and edit and courses pages

---
+++ src/app/[lang]/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import {type Locale } from "#/i18n.config";
+
+const DashboardPage = ({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) => {
+  return (
+    <div>dashboard</div>
+  );
+};
+export default DashboardPage;
