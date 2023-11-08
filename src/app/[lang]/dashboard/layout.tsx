a81a40b6e4960f0ff089c34fb8ec56fa56c329a5
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-07 23:35:03 -0800

    feat: dashboard sidebar

--- src/app/[lang]/dashboard/layout.tsx
+++ src/app/[lang]/dashboard/layout.tsx
@@ -1,4 +1,7 @@
-import { Locale } from "#/i18n.config";
+import { PanelAsideOption } from "@/components/pages/user-panel/panel-sidebar-items/panel-aside-option";
+import { PanelAvatar } from "@/components/pages/user-panel/panel-sidebar-items/panel-avatar";
+
+import { type Locale } from "#/i18n.config";
 
 const DashboardLayout = ({
   children,
@@ -8,12 +11,21 @@ const DashboardLayout = ({
   params: { lang: Locale };
 }) => {
   return (
-    <section className="h-[600px] w-full border border-black ">
-      <aside> sidebar</aside>
-      <section>
+    <div className="mt-10 flex flex-row lg:px-[5%] xl:px-[8%]">
+      <aside className="sticky  top-10 hidden h-full flex-col gap-10  lg:flex lg:w-1/4">
+        <div className="mt-5 flex w-10/12 gap-4 border border-black pr-2  ">
+          <PanelAvatar />
+        </div>
+        <div className="">
+          <PanelAsideOption lang={lang} />
+        </div>
+      </aside>
+
+      <div></div>
+      <section className="h-[900px] rounded-xl border border-black bg-card shadow md:w-full lg:w-3/4">
         {children}
       </section>
-    </section>
+    </div>
   );
 };
 export default DashboardLayout;
