e70f2d1d9e1d4ca0028a876b5a1c31c347ec2732
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-09 14:39:55 +0330

    fix: NEXT_AUTH_ envs renamed to AUTH_

--- src/core/config/env.ts
+++ src/core/config/env.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 const envSchema = z.object({
   BASE_API_URL: z.string().min(1),
-  NEXT_AUTH_SECRET: z.string().length(32),
-  NEXT_AUTH_URL: z
+  AUTH_SECRET: z.string().length(32),
+  AUTH_URL: z
     .string()
     .min(1)
     .refine((url) => url.endsWith("/api/auth")),
