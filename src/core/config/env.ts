b37358c55075fb207ae2933957426752b6eb244a
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-09 14:12:42 +0330

    chore: next-auth env variables

--- src/core/config/env.ts
+++ src/core/config/env.ts
@@ -2,6 +2,11 @@ import { z } from "zod";
 
 const envSchema = z.object({
   BASE_API_URL: z.string().min(1),
+  NEXT_AUTH_SECRET: z.string().length(32),
+  NEXT_AUTH_URL: z
+    .string()
+    .min(1)
+    .refine((url) => url.endsWith("/api/auth")),
 });
 
 export const parsedEnv = envSchema.parse(process.env);
