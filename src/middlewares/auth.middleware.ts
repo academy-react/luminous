2ef6e319f579f1aa560ce11fef49103a2a145900
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-10 13:14:10 +0330

    refactor: auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -9,13 +9,33 @@ import { auth } from "@/lib/auth";
 import { type MiddlewareFactory } from "./chain";
 
 export const withAuthNMiddleware: MiddlewareFactory = (nextMiddleware) => {
-  console.log("here");
-
   return async (req: NextRequest, _next: NextFetchEvent) => {
     const session = await auth();
 
-    console.log("session in middleware");
-    console.log(session);
+    const pathnameLocale = req.nextUrl.pathname.split("/")[1];
+    const pathnameWithoutLocale = req.nextUrl.pathname.replace(
+      `/${pathnameLocale}`,
+      ""
+    );
+
+    const isLoggedIn = !!session?.user;
+
+    const isAuthPage =
+      pathnameWithoutLocale.startsWith("/sign-in") ||
+      pathnameWithoutLocale.startsWith("/sign-up") ||
+      pathnameWithoutLocale.startsWith("/reset-password");
+
+    const isDashboardPage = pathnameWithoutLocale.startsWith("/dashboard");
+
+    if (isAuthPage && isLoggedIn) {
+      return NextResponse.redirect(new URL(`/${pathnameLocale}`, req.url));
+    }
+
+    if (isDashboardPage && !isLoggedIn) {
+      return NextResponse.redirect(
+        new URL(`/${pathnameLocale}/sign-in`, req.url)
+      );
+    }
 
     return nextMiddleware(req, _next);
   };
