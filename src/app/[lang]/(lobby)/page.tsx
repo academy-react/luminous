201102c271d921bba53a31b1e375080708bbe7c2
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-17 04:17:31 -0800

    refactor: add getCategories to page.tsx

--- src/app/[lang]/(lobby)/page.tsx
+++ src/app/[lang]/(lobby)/page.tsx
@@ -13,17 +13,18 @@ import { getTopCoursesList } from "@/core/services/api";
 
 import { type Locale } from "#/i18n.config";
 import { getAllTeacher } from "@/core/services/api/teacher/get-all-teachers";
+import { getCategories } from "@/core/services/api/category";
 
 const HomePage = async ({ params: { lang } }: { params: { lang: Locale } }) => {
   const courseData = await getTopCoursesList();
   const teacherData = await getAllTeacher();
-
+  const categoryData = await getCategories();
   return (
     <PageAnimationWrapper className="flex w-full flex-col items-center justify-center gap-10">
       <HeroSection lang={lang} />
       <ServiceSection lang={lang} />
       <CourseSection lang={lang} data={courseData}/>
-      <CategoriesSection lang={lang} />
+      <CategoriesSection lang={lang} data={categoryData}/>
       <TeachersSection lang={lang} data={teacherData} />
       <NewsSection lang={lang} />
       <TempTitle lang={lang} />
