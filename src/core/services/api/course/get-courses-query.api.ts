cd918f55929a8f890c6914ec749af0dfcf6879ff
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-18 04:06:59 -0800

    feat: course-paggination api and schema

---
+++ src/core/services/api/course/get-courses-query.api.ts
@@ -0,0 +1,21 @@
+
+import http from "@/core/services/interceptor";
+import { coursePaginationListSchema} from "@/core/validators/api";
+
+export const getCoursesByQuery = async (
+  query: string = ""
+) => {
+  const response = await http.get(
+    `/Home/GetCoursesWithPagination?Query=${query}`
+  );
+
+  const parsedResult = coursePaginationListSchema.safeParse(response.data);
+
+  if (!parsedResult.success) {
+    console.error(parsedResult.error);
+
+    return null;
+  }
+
+  return parsedResult.data;
+};
