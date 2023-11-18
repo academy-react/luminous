1176d76a845ac63ead77d1ecc691365645b52248
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-17 21:51:00 -0800

    fix: types

--- src/core/validators/api/teacher/teacher.schema.ts
+++ src/core/validators/api/teacher/teacher.schema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const teacherSchema = z.object({
     teacherId: z.number(),
-    fullName: z.string(),
-    linkdinProfileLink: z.string(),
+    fullName: z.string().nullable(),
+    linkdinProfileLink: z.string().nullable(),
     pictureAddress: z.string().nullable(),
     courseCounts: z.number(),
     newsCount: z.number()
