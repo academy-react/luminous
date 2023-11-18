5bc233d52c95b1ad6e467508690f05666b2e2a8f
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-18 10:54:22 -0800

    fix: delete consol.logs

--- src/core/services/api/course/get-top-courses.api.ts
+++ src/core/services/api/course/get-top-courses.api.ts
@@ -4,8 +4,6 @@ import { courseListSchema } from "@/core/validators/api";
 export const getTopCoursesList = async (count: number = 5) => {
   const response = await http.get(`/Home/GetCoursesTop?Count=${count}`);
   const parsedResult = courseListSchema.safeParse(response.data);
-  console.log("parsedResult:", parsedResult)
-  console.log("response:", response)
 
   if (!parsedResult.success) {
     console.error(parsedResult.error);
