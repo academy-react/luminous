79c3975ad96b5d1b76c46a26cd9b8373c92edbce
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-19 20:58:35 +0330

    fix: api schemas turned to strict

--- src/core/validators/api/news/news.schema.ts
+++ src/core/validators/api/news/news.schema.ts
@@ -1,21 +1,23 @@
 import { z } from "zod";
 
-export const newsSchema = z.object({
-  id: z.string(),
-  title: z.string(),
-  miniDescribe: z.string(),
-  keyword: z.string(),
-  currentRate: z.number(),
-  currentView: z.number(),
-  currentLikeCount: z.number(),
-  currentImageAddressTumb: z.string(),
-  insertDate: z.string(),
-  updateDate: z.string(),
-  addUserFullName: z.string(),
-  newsCatregoryId: z.number(),
-  newsCatregoryName: z.string(),
-  currentUserIsLike: z.boolean(),
-});
+export const newsSchema = z
+  .object({
+    id: z.string(),
+    title: z.string(),
+    miniDescribe: z.string(),
+    keyword: z.string(),
+    currentRate: z.number(),
+    currentView: z.number(),
+    currentLikeCount: z.number(),
+    currentImageAddressTumb: z.string(),
+    insertDate: z.string(),
+    updateDate: z.string(),
+    addUserFullName: z.string(),
+    newsCatregoryId: z.number(),
+    newsCatregoryName: z.string(),
+    currentUserIsLike: z.boolean(),
+  })
+  .strict();
 
 export type NewsType = z.infer<typeof newsSchema>;
 
