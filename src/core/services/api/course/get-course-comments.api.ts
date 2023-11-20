47a3dacc2b65c74e28050a2fc18a32234e8f81e1
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-20 13:55:54 +0330

    feat: course comment reply api

--- src/core/services/api/course/get-course-comments.api.ts
+++ src/core/services/api/course/get-course-comments.api.ts
@@ -1,10 +1,32 @@
 import http from "@/core/services/interceptor";
-import { courseCommentListSchema } from "@/core/validators/api/course";
+import {
+  courseCommentListSchema,
+  courseCommentReplyListSchema,
+} from "@/core/validators/api/course";
 
 export const getCourseComments = async (courseId: string) => {
   const response = await http.get(`/Course/GetCourseCommnets/${courseId}`);
 
   const parsedResult = courseCommentListSchema.safeParse(response.data);
+
+  if (!parsedResult.success) {
+    console.error(parsedResult.error);
+
+    return [];
+  }
+
+  return parsedResult.data;
+};
+
+export const getCourseCommentReplies = async (
+  courseId: string,
+  commentId: string
+) => {
+  const response = await http.get(
+    `/Course/GetCourseReplyComments/${courseId}/${commentId}`
+  );
+
+  const parsedResult = courseCommentReplyListSchema.safeParse(response.data);
 
   if (!parsedResult.success) {
     console.error(parsedResult.error);
