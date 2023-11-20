e0a83246d013c0aa92c2e87853fb85931d7b49b4
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-20 14:41:22 +0330

    feat: course comment replies turned dynamic

--- src/components/pages/details-page/comps/content-body/comment/comment.tsx
+++ src/components/pages/details-page/comps/content-body/comment/comment.tsx
@@ -2,8 +2,7 @@ import Image from "next/image";
 
 import { Button } from "@/components/elements/ui";
 
-import { type CommentDictProps } from "@/dict/dev/comments.dict";
-
+import { getCourseCommentReplies } from "@/core/services/api";
 import { type CourseCommentType } from "@/core/validators/api";
 
 import { type Locale } from "#/i18n.config";
@@ -13,8 +12,11 @@ type CommentProps = {
   comment: CourseCommentType;
 };
 
-export const Comment = ({ lang, comment }: CommentProps) => {
-  const replies = comment.acceptReplysCount > 0 ? [] : [];
+export const Comment = async ({ lang, comment }: CommentProps) => {
+  const replies =
+    comment.acceptReplysCount > 0
+      ? await getCourseCommentReplies(comment.courseId, comment.id)
+      : [];
 
   return (
     <div className="rounded-2xl bg-gray-100 p-5">
@@ -26,11 +28,36 @@ export const Comment = ({ lang, comment }: CommentProps) => {
         <div className="w-full">
           <UserInfo name={comment.author} date={comment.insertDate} />
           <CommentText text={comment.describe} />
-          <section className="mt-7 space-y-4">
-            {replies.map((reply, index) => (
-              <Reply key={index} lang={lang} reply={reply} />
-            ))}
-          </section>
+          {replies.length > 0 && (
+            <section className="mt-7 space-y-4">
+              {replies.map((reply, index) => (
+                <Reply key={index} lang={lang} reply={reply} />
+              ))}
+            </section>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Reply = ({
+  lang: _lang,
+  reply,
+}: {
+  lang: Locale;
+  reply: CourseCommentType;
+}) => {
+  return (
+    <div className="rounded-2xl bg-gray-200 p-5">
+      <div className="flex items-start gap-x-5">
+        <UserAvatar
+          avatar={"/images/dev/person-avatar.jpg"}
+          userType={"student"}
+        />
+        <div className="w-full">
+          <UserInfo name={reply.author} date={reply.insertDate} />
+          <CommentText text={reply.describe} />
         </div>
       </div>
     </div>
@@ -75,21 +102,4 @@ const UserInfo = ({ name, date }: { name: string; date: string }) => {
 
 const CommentText = ({ text }: { text: string }) => {
   return <p className="mt-3.5">{text}</p>;
-};
-
-const Reply = ({ lang, reply }: { lang: Locale; reply: CommentDictProps }) => {
-  return (
-    <div className="rounded-2xl bg-gray-200 p-5">
-      <div className="flex items-start gap-x-5">
-        <UserAvatar
-          avatar={reply.sender.avatar}
-          userType={reply.sender.userType}
-        />
-        <div className="w-full">
-          <UserInfo name={reply.sender.name[lang]} date={reply.date} />
-          <CommentText text={reply.text[lang]} />
-        </div>
-      </div>
-    </div>
-  );
 };
