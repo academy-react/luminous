1dd21d2defbbc01837b15fc4959dd3c35d73031c
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-21 01:39:20 -0800

    refactor: add useEffects and ...

--- src/components/layouts/header/search-nav.tsx
+++ src/components/layouts/header/search-nav.tsx
@@ -1,4 +1,8 @@
-import { useEffect, useState, useTransition } from "react";
+import { useCallback, useEffect, useState, useTransition } from "react";
+import { useRouter } from "next/navigation";
+
+import { useDebounce } from "use-debounce";
+import { string } from "zod";
 
 import { Button } from "@/components/elements/ui";
 import {
@@ -21,15 +25,39 @@ export const SearchNav = () => {
   const [courseList, setCourseList] = useState<
     CoursePaginationListType | null | undefined
   >();
-  const getCourseQuery = async () => {
-    const params = { Query: "jhjh" };
-    const courses = await getCoursesByQuery(params.Query);
-    setCourseList(courses);
-  };
+  const [query, setQuery] = useState("");
+  const debouncedQuery = useDebounce(query, 300);
+  const router = useRouter();
+  useEffect(() => {
+    if (debouncedQuery.length <= 0) {
+      setCourseList(null);
+      return;
+    }
+
+    const getCourse = async () => {
+      try {
+        const data = await getCoursesByQuery(debouncedQuery);
+        setCourseList(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    startTransition(getCourse);
+
+    return () => setCourseList(null);
+  }, [debouncedQuery]);
+
+  const handleSelect = useCallback((callback: () => unknown) => {
+    setOpen(false);
+    callback();
+  }, []);
 
   useEffect(() => {
-    void getCourseQuery();
-  }, []);
+    if (!open) {
+      setQuery("");
+    }
+  }, [open]);
 
   return (
     <>
@@ -40,7 +68,11 @@ export const SearchNav = () => {
         <Icons.search />
       </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput placeholder="جستجو بر اساس عنوان..." />
+        <CommandInput
+          placeholder="جستجو بر اساس عنوان..."
+          value={query}
+          onValueChange={setQuery}
+        />
         <CommandList>
           <CommandEmpty
             className={cn(isPending ? "hidden" : "py-6 text-center text-sm")}
@@ -48,28 +80,36 @@ export const SearchNav = () => {
             یافت نشد.
           </CommandEmpty>
           <div className="mx-auto flex justify-center ">
-            <CommandGroup
-              heading="دوره ها"
-              className="flex basis-1/2 flex-col items-center py-2 text-purple-primary"
-            >
-              {courseList?.map((item) => (
-                <CommandItem
-                  key={item.courseId}
-                  value={item.title}
-                  onSelect={
-                    () => {}
-                    // handleSelect(() => router.push(`/product/${item.id}`))
-                  }
-                >
-                  {item.title}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-
+            {isPending ? (
+              <div className="space-y-1 overflow-hidden px-1 py-2">
+                <Skeleton className="h-4 w-10 rounded" />
+                <Skeleton className="h-8 rounded-sm" />
+                <Skeleton className="h-8 rounded-sm" />
+              </div>
+            ) : (
+              <CommandGroup
+                heading="دوره ها"
+                className="flex basis-1/2 flex-col items-center py-2 text-purple-primary"
+              >
+                {courseList?.map((item) => (
+                  <CommandItem
+                    key={item.courseId}
+                    value={item.title}
+                    onSelect={() => {
+                      handleSelect(() =>
+                        router.push(`/courses/${item.courseId}`)
+                      );
+                    }}
+                  >
+                    {item.title}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            )}
             <div className="divide-x border "></div>
             <CommandGroup
               heading="اخبار و مقالات"
-              className="items-center flex basis-1/2 flex-col  py-2 text-purple-primary "
+              className="flex basis-1/2 flex-col items-center  py-2 text-purple-primary "
             >
               <CommandItem>Calculator</CommandItem>
             </CommandGroup>
