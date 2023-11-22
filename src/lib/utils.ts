9ba4518807b736106bb0c1a1ca2a9e492df1606a
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-23 02:46:41 +0330

    feat: handle date function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import { type Locale } from "#/i18n.config";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function handleDate(date: string, lang: Locale) {
+  const d = new Date(date);
+
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  
+  return d.toLocaleDateString(lang, options);
 }
