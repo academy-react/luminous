7afce72921b36377677eecd0d080b360433057c5
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-22 14:56:43 +0330

    feat: get course status api

--- src/core/validators/api/course/index.ts
+++ src/core/validators/api/course/index.ts
@@ -2,3 +2,4 @@ export * from "./course.schema";
 export * from "./course-pagination.schema";
 export * from "./course-id.schema";
 export * from "./course-comments.schema";
+export * from "./course-status.schema";
