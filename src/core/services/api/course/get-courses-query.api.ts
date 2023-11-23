24a100211db4a899c62a2ff954b43e88a2c4f2c7
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-23 17:45:07 +0330

    fix: get course query api

--- src/core/services/api/course/get-courses-query.api.ts
+++ src/core/services/api/course/get-courses-query.api.ts
@@ -1,12 +1,12 @@
 import http from "@/core/services/interceptor";
-import { coursePaginationListSchema } from "@/core/validators/api";
+import { courseByPaginationSchema } from "@/core/validators/api";
 
 export const getCoursesByQuery = async (query: string = "") => {
   const response = await http.get(
     `/Home/GetCoursesWithPagination?Query=${query}`
   );
 
-  const parsedResult = coursePaginationListSchema.safeParse(response.data);
+  const parsedResult = courseByPaginationSchema.safeParse(response.data);
 
   if (!parsedResult.success) {
     console.error("error in get-course-query.api", parsedResult.error.errors);
