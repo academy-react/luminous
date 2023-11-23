01b22f74205e759360952696cd4ee636d36fef9d
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-23 14:10:59 +0330

    refactor: add pagination to content list

--- src/components/elements/common/pagination.tsx
+++ src/components/elements/common/pagination.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
 import { Icons } from "@/components/assets/icons";
+import { cn } from "@/lib/utils";
 
-export const Pagination = () => {
+export const Pagination = ({className}:{className:string;}) => {
   return (
-    <nav>
-      <ul className="flex flex-row-reverse items-center gap-1">
+    <nav >
+      <ul className={cn(" bg-card rounded-xl h-10 px-5 flex flex-row-reverse items-center gap-1",className)}>
         <li>
           <Link href="#" className="px-2">
             <Icons.leftArrow />
