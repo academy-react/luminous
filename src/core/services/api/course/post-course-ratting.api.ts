5216369ee332687d19f78390c3311de7f6e56d8b
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-24 16:48:05 +0330

    fix: edit schema name

--- src/core/services/api/course/post-course-ratting.api.ts
+++ src/core/services/api/course/post-course-ratting.api.ts
@@ -1,12 +1,12 @@
 import http from "@/core/services/interceptor";
-import { coursePaginationListSchema} from "@/core/validators/api";
+import { courseByPaginationSchema } from "@/core/validators/api";
 
 export const setCourseRatting = async (obj) => {
   const response = await http.post(
    `/Course/SetCourseRating?CourseId=${1}&RateNumber=${3}`
   );
 
-  const parsedResult = coursePaginationListSchema.safeParse(response.data);//change schema
+  const parsedResult = courseByPaginationSchema.safeParse(response.data);//change schema
 
   if (!parsedResult.success) {
     console.error(parsedResult.error);
