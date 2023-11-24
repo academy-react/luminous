ca728f963e8c1f0e9de0b7afb6ae7a0bb67eb8d8
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-24 16:48:36 +0330

    refactor: add get couiorse by pagination

--- src/core/services/api/course/get-courses-query.api.ts
+++ src/core/services/api/course/get-courses-query.api.ts
@@ -1,15 +1,38 @@
 import http from "@/core/services/interceptor";
 import { courseByPaginationSchema } from "@/core/validators/api";
 
+
 export const getCoursesByQuery = async (currentPage:number = 1, query:string = "",)=> {
 
 
   const response = await http.get(
-    `/Home/GetCoursesWithPagination?PageNumber=${currentPage}${query!= "" && `&Query=${query}`}`
+    `/Home/GetCoursesWithPagination?PageNumber=${currentPage}${query!= "" ?`&Query=${query}`: ""}`
   );
 
   const parsedResult = courseByPaginationSchema.safeParse(response.data);
 
+  if (!parsedResult.success) {
+    console.error("error in get-course-query.api", parsedResult.error.errors);
+
+    return null;
+  }
+
+  return parsedResult.data;
+};
+
+// getCourseByPagination 
+
+export const getCourseByPagination = async ( query:string = "",)=> {
+
+
+  const response = await http.get(
+    `/Home/GetCoursesWithPagination?${query!= "" ?`&Query=${query}`: ""}`
+  );
+const data = response.data;
+console.log("data:" ,data);
+// const totalPages = Math.ceil(data.)
+
+  const parsedResult = courseByPaginationSchema.safeParse(response.data);
   if (!parsedResult.success) {
     console.error("error in get-course-query.api", parsedResult.error.errors);
 
