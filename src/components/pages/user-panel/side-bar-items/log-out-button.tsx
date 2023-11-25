e101cff0b5b8734d5e8e6cff0e58a03d74b64b78
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-25 13:47:29 +0330

    refactor: minor changes dashboard nav-bar

--- src/components/pages/user-panel/side-bar-items/log-out-button.tsx
+++ src/components/pages/user-panel/side-bar-items/log-out-button.tsx
@@ -4,9 +4,9 @@ import { type Locale } from "#/i18n.config";
 
 export const LogOutButton = ({ lang }: { lang: Locale }) => {
   return (
-    <div className="flex items-center gap-2 py-7">
-      <Icons.logOut className="text-white" />
-      <span className="text-white">
+    <div className="flex items-center gap-2 py-7 font-bold text-white">
+      <Icons.logOut className="" />
+      <span className="">
         {
           {
             fa: "خروج از حساب کاربری",
