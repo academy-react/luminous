64da2b33153336c1961f9df51373bead6d1e27d0
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-25 12:07:14 +0330

    fix: add use server

--- src/core/services/api/course/get-courses-query.api.ts
+++ src/core/services/api/course/get-courses-query.api.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import http from "@/core/services/interceptor";
 import { courseByPaginationSchema } from "@/core/validators/api";
 type GetCoursesQueryParams={
@@ -23,25 +25,3 @@ const finalQuery = query!= "" ?`&Query=${query}`: ""
   return parsedResult.data;
 };
 
-// getCourseByPagination 
-
-// export const getCourseByPagination = async ( query:string = "",)=> {
-
-
-//   const response = await http.get(
-//     `/Home/GetCoursesWithPagination?${query!= "" ?`&Query=${query}`: ""}`
-//   );
-// const data = response.data;
-// console.log("data:" ,data);
-// // const totalPages = Math.ceil(data.)
-
-//   const parsedResult = courseByPaginationSchema.safeParse(response.data);
-
-//   if (!parsedResult.success) {
-//     console.error("error in get-course-query.api", parsedResult.error.errors);
-
-//     return null;
-//   }
-
-//   return parsedResult.data;
-// };
