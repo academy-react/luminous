1961fe7c811df8a0b8b5b29847077e174bb3c29c
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-25 16:11:15 +0330

    feat: online indicator on dashboard avatar

--- src/components/pages/user-panel/side-bar-items/panel-avatar.tsx
+++ src/components/pages/user-panel/side-bar-items/panel-avatar.tsx
@@ -6,7 +6,7 @@ import { type Locale } from "#/i18n.config";
 export const PanelAvatar = ({ lang }: { lang: Locale }) => {
   return (
     <div className="flex w-fit flex-col items-center justify-center gap-3 p-7">
-      <div className="relative aspect-square h-24">
+      <div className="relative aspect-square h-24 rounded-full">
         {/* <Image
           src="/images/dev/node-red.png"
           fill
@@ -14,8 +14,9 @@ export const PanelAvatar = ({ lang }: { lang: Locale }) => {
           className="rounded-full object-fill"
         /> */}
         <div className="flex h-full items-center justify-center rounded-full bg-gray-200">
-          <Icons.placeholder className="h-7 w-7 text-slate-500" />
+          <Icons.placeholder className="h-7 w-7 text-slate-400" />
         </div>
+        <div className="absolute bottom-0 start-0 h-3 w-3 -translate-x-2 -translate-y-2 rounded-full bg-green-500" />
       </div>
       <div className="flex flex-col justify-center gap-1 text-sm">
         <H1 className="font-extrabold text-white">
