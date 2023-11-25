ee3e5302e097e24539356c6dca6a108c85a0a463
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-25 21:17:19 +0330

    refactor: user dashboard side bar

--- src/app/[lang]/dashboard/layout.tsx
+++ src/app/[lang]/dashboard/layout.tsx
@@ -1,22 +1,36 @@
-import { LogOutButton } from "@/components/pages/user-panel/side-bar-items/log-out-button";
-import { PanelAvatar } from "@/components/pages/user-panel/side-bar-items/panel-avatar";
-import { SideBarNav } from "@/components/pages/user-panel/side-bar-items/side-nav-bar";
+import {
+  LogOutButton,
+  UserAvatar,
+} from "@/components/pages/user-panel/side-bar-items";
+import { SideBarNav } from "@/components/pages/user-panel/side-bar-items/client";
+
+import { getUserProfileInfo } from "@/core/services/api";
 
 import { type Locale } from "#/i18n.config";
 
-const DashboardLayout = ({
+const DashboardLayout = async ({
   children,
   params: { lang },
 }: {
   children: React.ReactNode;
   params: { lang: Locale };
 }) => {
+  const data = await getUserProfileInfo();
+
+  console.log(data);
+
+  if (!data) return null;
+
   return (
     <div className="flex h-screen max-h-screen min-h-screen w-full items-center px-24 py-16">
       <div className="flex h-full w-full rounded-2xl bg-primary py-3 pe-3">
         <aside className="hidden h-full flex-col items-center justify-between lg:flex lg:w-1/4">
           <div className="flex w-full flex-col items-center">
-            <PanelAvatar lang={lang} />
+            <UserAvatar
+              fName={data.fName}
+              lName={data.lName}
+              picture={data.currentPictureAddress}
+            />
             <SideBarNav lang={lang} />
           </div>
           <LogOutButton lang={lang} />
