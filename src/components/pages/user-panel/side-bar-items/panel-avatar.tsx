a77625e4885e837cdb1b56ddca233d480488cc09
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-25 13:47:29 +0330

    refactor: minor changes dashboard nav-bar

--- src/components/pages/user-panel/side-bar-items/panel-avatar.tsx
+++ src/components/pages/user-panel/side-bar-items/panel-avatar.tsx
@@ -5,8 +5,8 @@ import { type Locale } from "#/i18n.config";
 
 export const PanelAvatar = ({ lang }: { lang: Locale }) => {
   return (
-    <div className="flex w-fit flex-col items-center justify-center gap-2 p-7">
-      <div className="relative aspect-square h-28">
+    <div className="flex w-fit flex-col items-center justify-center gap-3 p-7">
+      <div className="relative aspect-square h-24">
         {/* <Image
           src="/images/dev/node-red.png"
           fill
@@ -14,11 +14,11 @@ export const PanelAvatar = ({ lang }: { lang: Locale }) => {
           className="rounded-full object-fill"
         /> */}
         <div className="flex h-full items-center justify-center rounded-full bg-gray-200">
-          <Icons.placeholder className="h-10 w-10 text-slate-500" />
+          <Icons.placeholder className="h-7 w-7 text-slate-500" />
         </div>
       </div>
       <div className="flex flex-col justify-center gap-1 text-sm">
-        <H1 className="font-semibold text-white">
+        <H1 className="font-extrabold text-white">
           {{ fa: "فرشته احمدی", en: "Fereshteh Ahmadi" }[lang]}
         </H1>
       </div>
