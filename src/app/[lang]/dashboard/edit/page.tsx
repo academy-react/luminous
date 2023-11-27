283175334cca7a829898d0ee826a634dc79880a8
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-27 12:14:56 +0330

    refactor: minor changes to dashboard account

--- src/app/[lang]/dashboard/edit/page.tsx
+++ src/app/[lang]/dashboard/edit/page.tsx
@@ -1,10 +1,10 @@
 import { type Locale } from "#/i18n.config";
 
-const EditProfile = ({ params: { lang } }: { params: { lang: Locale } }) => {
+const ProfileDetails = ({ params: { lang } }: { params: { lang: Locale } }) => {
   return (
     <div className="flex h-full w-full items-center justify-center">
       edit profile
     </div>
   );
 };
-export default EditProfile;
+export default ProfileDetails;
--- src/components/pages/user-panel/account-page/latest-posts-card.tsx
+++ src/components/pages/user-panel/account-page/latest-posts-card.tsx
@@ -12,26 +12,8 @@ export const LatestUpdatesCard = ({
     update: string;
   };
 }) => {
-  const colors = [
-    "bg-emerald-400",
-    "bg-blue-400",
-    "bg-yellow-400",
-    "bg-red-400",
-    "bg-pink-400",
-    "bg-purple-400",
-    "bg-indigo-400",
-    "bg-green-400",
-    "bg-gray-400",
-    "bg-black",
-  ];
-
   return (
-    <div
-      className={cn(
-        "flex w-60 snap-start flex-col gap-2 rounded-2xl bg-emerald-400 p-4",
-        colors[index % colors.length]
-      )}
-    >
+    <div className="flex w-60 snap-start flex-col gap-2 rounded-2xl bg-emerald-400 p-4">
       <div className="flex items-center justify-between">
         <span className="text-xs font-extrabold text-white">
           {post.courseTitle}
