52ff7bd100f6427fedf999bbda5bd8eaede45238
freshte.ahmadi69 <freshte.ahmadi69@gmail.com>
2023-11-28 12:38:04 +0330

    feat: rating schema

--- src/core/services/api/course/post/post-course-ratting.api.ts
+++ src/core/services/api/course/post/post-course-ratting.api.ts
@@ -1,14 +1,14 @@
 "use server";
 
 import http from "@/core/services/interceptor";
-import { courseByPaginationSchema } from "@/core/validators/api";
+import { courseRatingResponseSchema } from "@/core/validators/api";
 
 export const setCourseRatting = async (id: string, rating: number) => {
   const response = await http.post(
     `/Course/SetCourseRating?CourseId=${id}&RateNumber=${rating}`
   );
 
-  const parsedResult = courseByPaginationSchema.safeParse(response.data); //change schema
+  const parsedResult = courseRatingResponseSchema.safeParse(response.data); //change schema
 
   if (!parsedResult.success) {
     console.error(parsedResult.error);
