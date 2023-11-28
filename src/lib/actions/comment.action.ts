978d96b2e0f411dcb96d7982b1852b652a9f06ba
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-28 10:15:56 +0330

    feat: course comment dislike

--- src/lib/actions/comment.action.ts
+++ src/lib/actions/comment.action.ts
@@ -2,6 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 
+import { addCourseCommentDissLike } from "@/core/services/api/course/post/post-course-comment-diss-like.api";
 import { addCourseCommentLike } from "@/core/services/api/course/post/post-course-comment-like.api";
 
 import { type Locale } from "#/i18n.config";
@@ -12,7 +13,7 @@ export const courseCommentLikeAction = async (
   lang: Locale
 ) => {
   console.log(commentId);
-  
+
   const res = await addCourseCommentLike(commentId);
 
   console.log(res);
@@ -27,14 +28,10 @@ export const courseCommentDissLikeAction = async (
   commentId: string,
   lang: Locale
 ) => {
-  console.log(commentId);
-  
-  const res = await addCourseCommentLike(commentId);
-
-  console.log(res);
+  const res = await addCourseCommentDissLike(commentId);
+  const res2 = await addCourseCommentDissLike(commentId);
 
   revalidatePath(`${lang}/courses/${courseId}`);
 
   return res;
 };
-
