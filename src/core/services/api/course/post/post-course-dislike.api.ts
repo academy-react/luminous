adb0123ecbbb43b2249af9e428853e49c09d1ccd
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-11-30 17:06:22 +0330

    fix: course dislike naming

--- src/core/services/api/course/post/post-course-dislike.api.ts
+++ src/core/services/api/course/post/post-course-dislike.api.ts
@@ -1,7 +1,7 @@
 import http from "@/core/services/interceptor";
 import { courseLikeResponseSchema } from "@/core/validators/api";
 
-export const addCourseDissLike = async (courseId: number | string) => {
+export const addCourseDislike = async (courseId: number | string) => {
   const response = await http.post(
     `/Course/AddCourseDissLike?CourseCommandId=${courseId}`
   );
