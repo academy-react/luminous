ca1d570b3c41dc4e36beef9818409be58e3b5e3f
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-12-05 02:46:04 +0330

    refactor: course api and schema folders structure

--- src/app/[lang]/(lobby)/page.tsx
+++ src/app/[lang]/(lobby)/page.tsx
@@ -9,21 +9,24 @@ import {
 import { TeachersSection } from "@/components/pages/lobby/client";
 import { TempTitle } from "@/components/utils/temp-title";
 
-import { getAllTeacher, getTopCoursesList } from "@/core/services/api";
-import { getCategories } from "@/core/services/api/category";
+import {
+  getAllTeacher,
+  getCourseCategories,
+  getTopCoursesList,
+} from "@/core/services/api";
 
 import { type Locale } from "#/i18n.config";
 
 const HomePage = async ({ params: { lang } }: { params: { lang: Locale } }) => {
   const courseData = await getTopCoursesList();
   const teacherData = await getAllTeacher();
-  const categoryData = await getCategories();
+  const categoryData = await getCourseCategories();
   return (
     <PageAnimationWrapper className="flex w-full flex-col items-center justify-center gap-10">
       <HeroSection lang={lang} />
       <ServiceSection lang={lang} />
-      <CourseSection lang={lang} data={courseData}/>
-      <CategoriesSection lang={lang} data={categoryData}/>
+      <CourseSection lang={lang} data={courseData} />
+      <CategoriesSection lang={lang} data={categoryData} />
       <TeachersSection lang={lang} data={teacherData} />
       <NewsSection lang={lang} />
       <TempTitle lang={lang} />
