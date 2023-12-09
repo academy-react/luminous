9248d1777c684c1544cb9b8a51b285a1d7cdb774
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-12-10 01:35:56 +0330

    refactor: categories data fetching moved into component

--- src/app/[lang]/(lobby)/page.tsx
+++ src/app/[lang]/(lobby)/page.tsx
@@ -9,23 +9,19 @@ import {
 import { TeachersSection } from "@/components/pages/lobby/client";
 import { TempTitle } from "@/components/utils/temp-title";
 
-import {
-  getCourseCategories,
-  getTopCoursesList,
-} from "@/core/services/api";
+import { getTopCoursesList } from "@/core/services/api";
 
 import { type Locale } from "#/i18n.config";
 
 const HomePage = async ({ params: { lang } }: { params: { lang: Locale } }) => {
   const courseData = await getTopCoursesList();
-  const categoryData = await getCourseCategories();
 
   return (
     <PageAnimationWrapper className="flex w-full flex-col items-center justify-center gap-10">
       <HeroSection lang={lang} />
       <ServiceSection lang={lang} />
       <CourseSection lang={lang} data={courseData} />
-      <CategoriesSection lang={lang} data={categoryData} />
+      <CategoriesSection lang={lang} />
       <TeachersSection lang={lang} />
       <NewsSection lang={lang} />
       <TempTitle lang={lang} />
