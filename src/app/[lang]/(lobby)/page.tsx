8674428af540e04b324ff7faa37b9cc1f82a65d5
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-12-10 01:33:38 +0330

    refactor: teachers data fetching moved into component

--- src/app/[lang]/(lobby)/page.tsx
+++ src/app/[lang]/(lobby)/page.tsx
@@ -10,7 +10,6 @@ import { TeachersSection } from "@/components/pages/lobby/client";
 import { TempTitle } from "@/components/utils/temp-title";
 
 import {
-  getAllTeacher,
   getCourseCategories,
   getTopCoursesList,
 } from "@/core/services/api";
@@ -19,15 +18,15 @@ import { type Locale } from "#/i18n.config";
 
 const HomePage = async ({ params: { lang } }: { params: { lang: Locale } }) => {
   const courseData = await getTopCoursesList();
-  const teacherData = await getAllTeacher();
   const categoryData = await getCourseCategories();
+
   return (
     <PageAnimationWrapper className="flex w-full flex-col items-center justify-center gap-10">
       <HeroSection lang={lang} />
       <ServiceSection lang={lang} />
       <CourseSection lang={lang} data={courseData} />
       <CategoriesSection lang={lang} data={categoryData} />
-      <TeachersSection lang={lang} data={teacherData} />
+      <TeachersSection lang={lang} />
       <NewsSection lang={lang} />
       <TempTitle lang={lang} />
     </PageAnimationWrapper>
