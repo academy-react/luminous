96f15276fc38bb60e78c2c99053e28228d5d6ae8
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-12-17 08:30:50 +0330

    feat: setting button

--- src/components/utils/temp-title.tsx
+++ src/components/utils/temp-title.tsx
@@ -10,20 +10,18 @@ import { cn } from "@/lib/utils";
 import { type Locale } from "#/i18n.config";
 
 export const TempTitle = ({ lang }: { lang: Locale }) => {
-  const { theme, setTheme } = useTheme();
-
   return (
     <div className="flex w-full items-center justify-center gap-2 font-black">
       <h1 className={cn(lang === "en" ? "text-sm" : "text-xl", "h-fit")}>
         {titleDict.title[lang]}
       </h1>
-      <div
+      {/* <div
         className="h-fit cursor-pointer transition-all duration-500 ease-in-out hover:scale-110 hover:text-slate-400 active:scale-90"
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       >
         <MdLightMode className="block dark:hidden" />
         <MdOutlineDarkMode className="hidden dark:block" />
-      </div>
+      </div> */}
     </div>
   );
 };
