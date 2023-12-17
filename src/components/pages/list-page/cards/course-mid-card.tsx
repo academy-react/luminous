502e6cf30a00df506049ecebe9b9748b7f875bfd
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-12-17 11:13:17 +0330

    feat: course carousel

--- src/components/pages/list-page/cards/course-mid-card.tsx
+++ src/components/pages/list-page/cards/course-mid-card.tsx
@@ -8,7 +8,7 @@ import { Icons } from "@/components/assets/icons";
 
 import { courseFavoriteAction } from "@/core/actions";
 import { type CourseFilterDtoType } from "@/core/validators/api";
-import { cn } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 
 import { type Locale } from "#/i18n.config";
 
@@ -33,11 +33,10 @@ export const CourseMidCard = ({
     const res = await courseFavoriteAction(courseId, lang);
   };
   return (
-    <div className="flex w-full flex-col gap-2 rounded-xl bg-card px-4 py-2  shadow-lg">
+    <div className="flex h-full w-full flex-col gap-2 rounded-xl bg-card px-4 py-2  shadow-lg">
       <Link
         href={`/${lang}/courses/${courseId}`}
-        className=" relative flex aspect-video h-[200px] w-full items-center justify-center
-        rounded-xl bg-gray-200"
+        className=" relative flex aspect-video h-[200px] w-full items-center justify-center rounded-xl bg-gray-200"
       >
         {/* <Icons.placeholder className=" h-8 w-8 text-slate-600" /> */}
         {/* <Image
@@ -50,16 +49,19 @@ export const CourseMidCard = ({
       <div className=" flex flex-col gap-4 ">
         <div className="flex flex-row items-center justify-between">
           <div className="flex flex-row flex-wrap gap-2">
-            {technologyList.split(",").map((tech, index) => (
-              <div
-                key={index}
-                className="w-fit rounded-md bg-secondary px-2 py-1 text-xs text-primary"
-              >
-                {tech}
-              </div>
-            ))}
+            {technologyList
+              .split(",")
+              .slice(0, 3)
+              .map((tech, index) => (
+                <div
+                  key={index}
+                  className="w-fit rounded-md bg-secondary px-2 py-1 text-xs text-primary"
+                >
+                  {tech}
+                </div>
+              ))}
           </div>
-          <Button className="" onClick={() => void favoriteAction()}>
+          <Button className="p-0" onClick={() => void favoriteAction()}>
             <Icons.heart
               className={cn(
                 "h-6 w-6 text-slate-600 transition-all duration-500 hover:fill-slate-600",
@@ -71,7 +73,16 @@ export const CourseMidCard = ({
 
         <h2 className="text-xl font-semibold ">{title}</h2>
         <div className="flex flex-col gap-y-4 rounded-xl">
-          <div>{teacherName}</div>
+          <div className="flex flex-wrap items-center justify-between text-xs text-slate-500 dark:text-slate-400">
+            <div className="flex cursor-pointer items-center gap-x-1 transition-colors hover:text-primary">
+              <Icons.user className="h-4 w-4" />
+              <p>{teacherName}</p>
+            </div>
+            <div className="flex items-center gap-x-1">
+              <Icons.calender className="h-4 w-4 fill-slate-400" />
+              <p className="">{formatDate("2021-09-01T12:00:00", lang)}</p>
+            </div>
+          </div>
           <ul className="flex items-baseline justify-between">
             <li className="flex flex-row items-center text-base text-yellow-500">
               <span className=" text-3xl text-yellow-500 ">★</span>
--- src/components/pages/lobby/courses/course-section.tsx
+++ src/components/pages/lobby/courses/course-section.tsx
@@ -8,12 +8,14 @@ import { Button } from "@/components/elements/ui";
 
 import { coursesTitleBoxDict } from "@/dict/pages/lobby.dict";
 
-import { getTopCoursesList } from "@/core/services/api";
+import { getCoursesByPagination, getTopCoursesList } from "@/core/services/api";
 
 import { type Locale } from "#/i18n.config";
 
+import { CourseCarousel } from "./course-caraousel";
+
 export const CourseSection = async ({ lang }: { lang: Locale }) => {
-  const data = await getTopCoursesList();
+  const data = await getCoursesByPagination();
 
   return (
     <section className="relative mt-24">
@@ -29,41 +31,7 @@ export const CourseSection = async ({ lang }: { lang: Locale }) => {
             lang={lang}
           />
         ) : (
-          <div className="container flex w-full max-w-screen-lg flex-col gap-4 md:flex-row md:items-center">
-            <div className="h-full grow md:w-[70%]">
-              {data[0] && <CourseCard lang={lang} data={data[0]} />}
-            </div>
-            <div className="grow rounded-xl bg-card shadow">
-              <div className="flex h-full w-full items-center justify-center gap-4 p-4 md:flex-col">
-                <div className="hidden w-full flex-col items-center justify-center gap-4 md:flex">
-                  {/* new */}
-                  {data.slice(1, 4).map((course, index) => (
-                    <CourseSideCard key={index} img={course.tumbImageAddress} />
-                  ))}
-                </div>
-                <Button className="w-full max-w-fit rounded-lg border-2 border-primary bg-primary p-3 text-secondary duration-300 hover:bg-card hover:text-primary">
-                  <Link href="#" className="">
-                    <p className="hidden md:block">
-                      {
-                        {
-                          fa: "دوره‌های بیشتر",
-                          en: "More Courses",
-                        }[lang]
-                      }
-                    </p>
-                    <p className="md:hidden">
-                      {
-                        {
-                          fa: "بیشتر",
-                          en: "More",
-                        }[lang]
-                      }
-                    </p>
-                  </Link>
-                </Button>
-              </div>
-            </div>
-          </div>
+          <CourseCarousel lang={lang} data={data} />
         )}
       </div>
     </section>
