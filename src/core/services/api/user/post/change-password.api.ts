b2309ac431a24e36823b223407a804027cf022bf
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-12-17 09:47:27 +0330

    feat: change password

---
+++ src/core/services/api/user/post/change-password.api.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import http from "@/core/services/interceptor";
+import { loginResponseSchema } from "@/core/validators/api";
+import { ChangePasswordInputProps, type SignInInputProps } from "@/core/validators/forms";
+
+export const changePassword = async (credentials: ChangePasswordInputProps) => {
+  const response = await http.post("/SharePanel/ChangePassword", credentials);
+
+  const parsedResult = loginResponseSchema.safeParse(response.data);
+
+  if (!parsedResult.success) {
+    console.log(response.data);
+    console.error("error in change-password.api", parsedResult.error.errors);
+
+    return null;
+  }
+
+  return parsedResult.data;
+};
---
+++ src/core/validators/forms/dashboard/change-password.schema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const changePasswordInputSchema = z.object({
+  oldPassword: z.string(),
+  newPassword: z.string(),
+});
+
+export type ChangePasswordInputProps = z.infer<typeof changePasswordInputSchema>;
