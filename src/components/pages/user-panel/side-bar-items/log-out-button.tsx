2cbcaa78aff8047d7da46a6482a24d563aae8ac2
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-12-17 09:57:12 +0330

    fix: log out button on dashboard

--- src/components/pages/user-panel/side-bar-items/log-out-button.tsx
+++ src/components/pages/user-panel/side-bar-items/log-out-button.tsx
@@ -1,19 +1,35 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/elements/ui";
 import { Icons } from "@/components/assets/icons";
+
+import { logOutAction } from "@/core/actions";
 
 import { type Locale } from "#/i18n.config";
 
 export const LogOutButton = ({ lang }: { lang: Locale }) => {
+  const router = useRouter();
+
+  const handleLogOut = async () => {
+    const res = await logOutAction();
+
+    if (res) {
+      router.push(`/${lang}`);
+    }
+  };
   return (
     <div className="flex items-center gap-2 py-7 font-bold text-white">
       <Icons.logOut className="" />
-      <span className="">
+      <Button className="" onClick={() => void handleLogOut()}>
         {
           {
             fa: "خروج از حساب کاربری",
             en: "Exit",
           }[lang]
         }
-      </span>
+      </Button>
     </div>
   );
 };
